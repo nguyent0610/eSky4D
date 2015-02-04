bdca5e63240a9f1dd55ed8f0ded81439f753406a
nhanlt <nhanlt@hqsoft.tfs.local>
2015-02-04 08:20:00 +0000

    git-tfs-id: [https://hqsofttfs.hqsoft.com.vn/tfs/HQSOFT]$/Cloud/eSky4D;C29457

--- AP/AP10300/AP10300/Scripts/Screen/AP10300.debug.js
+++ AP/AP10300/AP10300/Scripts/Screen/AP10300.debug.js
@@ -536,10 +536,12 @@ var Focus3_Change = function (sender, e) {
 var checkRequire = function (items) {
     if (items != undefined) {
         for (var i = 0; i < items.length; i++) {
-            if (items[i]["TranAmt"] == undefined)
-                continue;
-            if (items[i]["TranAmt"] == "")
-                continue;
+            //if (items[i]["TranAmt"] == undefined)
+            //    continue;
+            //if (items[i]["TranAmt"] == "")
+            //    continue;
+            if (HQ.grid.checkRequirePass(items[i], keys)) continue;
+
 
             if (items[i]["TranAmt"] == 0) {
                  HQ.message.show(15, '@Util.GetLang("TranAmt")', null);
