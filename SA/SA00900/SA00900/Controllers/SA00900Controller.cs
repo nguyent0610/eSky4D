3311daa5db2f759a5cc47076f6d605a967682e1e
trunght:1 <trunght:1@hqsoft.tfs.local>
2015-02-05 02:39:33 +0000

    Cache theo ngôn ngữ
    git-tfs-id: [https://hqsofttfs.hqsoft.com.vn/tfs/HQSOFT]$/Cloud/eSky4D;C29483

--- SA/SA00900/SA00900/Controllers/SA00900Controller.cs
+++ SA/SA00900/SA00900/Controllers/SA00900Controller.cs
@@ -29,8 +29,8 @@ namespace SA00900.Controllers
             return View();
         }
 
-        [OutputCache(Duration = 1000000, VaryByParam = "none")]
-        public PartialViewResult Body()
+        [OutputCache(Duration = 1000000, VaryByParam = "lang")]
+        public PartialViewResult Body(string lang)
         {
             return PartialView();
         }
