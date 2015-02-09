3754feaea23e9c46b673942e81c2c0800d4b7278
trunght:1 <trunght:1@hqsoft.tfs.local>
2015-02-09 03:58:28 +0000

    check in for PHUCHD
    git-tfs-id: [https://hqsofttfs.hqsoft.com.vn/tfs/HQSOFT]$/Cloud/eSky4D;C29569

---
+++ SA/SA00400/SA00400/Controllers/SA00400Controller.cs
@@ -0,0 +1,113 @@
+using HQ.eSkyFramework;
+using Ext.Net;
+using Ext.Net.MVC;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PartialViewResult = System.Web.Mvc.PartialViewResult;
+using System.IO;
+using System.Text;
+namespace SA00400.Controllers
+{
+    [DirectController]
+    [CustomAuthorize]
+    [CheckSessionOut]
+    public class SA00400Controller : Controller
+    {
+        private string _screenNbr = "SA00400";
+        private string _userName = Current.UserName;
+
+        SA00400Entities _db = Util.CreateObjectContext<SA00400Entities>(true);
+
+        public ActionResult Index()
+        {  
+            Util.InitRight(_screenNbr);
+            return View();
+        }
+
+       //[OutputCache(Duration = 1000000, VaryByParam = "lang")]
+        public PartialViewResult Body(string lang)
+        {
+            return PartialView();
+        }
+
+        public ActionResult GetData()
+        {
+            return this.Store(_db.SA00400_pgLoadGrid().ToList());
+        }
+
+        [HttpPost]
+        public ActionResult Save(FormCollection data)
+        {
+            try
+            {
+
+                StoreDataHandler dataHandler = new StoreDataHandler(data["lstData"]);
+                ChangeRecords<SYS_ScreenCat> lstLang = dataHandler.BatchObjectData<SYS_ScreenCat>();
+                foreach (SYS_ScreenCat deleted in lstLang.Deleted)
+                {
+                    var del = _db.SYS_ScreenCat.Where(p => p.CatID == deleted.CatID).FirstOrDefault();
+                    if (del != null)
+                    {
+                        _db.SYS_ScreenCat.DeleteObject(del);
+                    }
+                }
+
+                lstLang.Created.AddRange(lstLang.Updated);
+
+                foreach (SYS_ScreenCat curLang in lstLang.Created)
+                {
+                    if (curLang.CatID.PassNull() == "") continue;
+
+                    var lang = _db.SYS_ScreenCat.Where(p => p.CatID.ToLower() == curLang.CatID.ToLower()).FirstOrDefault();
+
+                    if (lang != null)
+                    {
+                        if (lang.tstamp.ToHex() == curLang.tstamp.ToHex())
+                        {
+                            Update_Language(lang, curLang, false);
+                        }
+                        else
+                        {
+                            throw new MessageException(MessageType.Message, "19");
+                        }
+                    }
+                    else
+                    {
+                        lang = new SYS_ScreenCat();
+                        Update_Language(lang, curLang, true);
+                        _db.SYS_ScreenCat.AddObject(lang);
+                    }
+                }
+
+                _db.SaveChanges();
+                return Json(new { success = true });
+            }
+            catch (Exception ex)
+            {
+                if (ex is MessageException) return (ex as MessageException).ToMessage();
+                return Json(new { success = false, type = "error", errorMsg = ex.ToString() });
+            }
+        }
+
+        private void Update_Language(SYS_ScreenCat t, SYS_ScreenCat s, bool isNew)
+        {
+            if (isNew)
+            {
+                t.CatID = s.CatID;
+                t.Crtd_Datetime = DateTime.Now;
+                t.Crtd_Prog = _screenNbr;
+                t.Crtd_User = _userName;
+            }
+
+            t.Descr = s.Descr;
+
+            t.LUpd_Datetime = DateTime.Now;
+            t.LUpd_Prog = _screenNbr;
+            t.LUpd_User = _userName;
+        }
+    }
+}
---
+++ SA/SA00400/SA00400/Global.asax
@@ -0,0 +1,1 @@
+<%@ Application Codebehind="Global.asax.cs" Inherits="SA00400.MvcApplication" Language="C#" %>
