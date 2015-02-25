47d0a782ae2c0b93210994ea860fe8c5d750a211
sonnph:2 <sonnph:2@hqsoft.tfs.local>
2015-02-25 02:13:41 +0000

    git-tfs-id: [https://hqsofttfs.hqsoft.com.vn/tfs/HQSOFT]$/Cloud/eSky4D;C29676

--- AP/AP20200/AP20200/Properties/AssemblyInfo.cs
+++ AP/AP20200/AP20200/Properties/AssemblyInfo.cs
@@ -6,14 +6,13 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("AP20200")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Tạo chỉnh nhà cung cấp")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("AP20200")]
+[assembly: AssemblyCompany("Han Quang Software Solution")]
+[assembly: AssemblyProduct("HQ Solutions - Cloud Framework")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyTrademark("Han Quang Software Solution. All rights Reserved.")]
 [assembly: AssemblyCulture("")]
-
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
