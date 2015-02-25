23290d7644f1111e32ba07c765e64c896d9517fa
phuchd:1 <phuchd:1@hqsoft.tfs.local>
2015-02-25 01:56:42 +0000

    git-tfs-id: [https://hqsofttfs.hqsoft.com.vn/tfs/HQSOFT]$/Cloud/eSky4D;C29672

---
+++ SA/SA02100/SA02100/Controllers/SA02100Controller.cs
@@ -0,0 +1,113 @@
+using HQ.eSkyFramework;
+using Ext.Net;
+using Ext.Net.MVC;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PartialViewResult = System.Web.Mvc.PartialViewResult;
+using System.IO;
+using System.Text;
+namespace SA02100.Controllers
+{
+    [DirectController]
+    [CustomAuthorize]
+    [CheckSessionOut]
+    public class SA02100Controller : Controller
+    {
+        private string _screenNbr = "SA02100";
+        private string _userName = Current.UserName;
+        SA02100Entities _db = Util.CreateObjectContext<SA02100Entities>(true);
+        public ActionResult Index()
+        {
+            
+            Util.InitRight(_screenNbr);
+            return View();
+        }
+        //[OutputCache(Duration = 1000000, VaryByParam = "lang")]
+        public PartialViewResult Body(string lang)
+        {
+            return PartialView();
+        }
+        public ActionResult GetSYS_RibbonScreen()
+        {
+            return this.Store(_db.SA02100_pgSYS_RibbonScreen().ToList());
+        }
+        [HttpPost]
+        public ActionResult Save(FormCollection data)
+        {
+            try
+            {
+                StoreDataHandler dataHandler = new StoreDataHandler(data["lstSYS_RibbonScreen"]);
+                ChangeRecords<SA02100_pgSYS_RibbonScreen_Result> lstSYS_RibbonScreen = dataHandler.BatchObjectData<SA02100_pgSYS_RibbonScreen_Result>();
+                foreach (SA02100_pgSYS_RibbonScreen_Result deleted in lstSYS_RibbonScreen.Deleted)
+                {
+                    var del = _db.SYS_RibbonScreen.Where(p => p.ScreenNumber == deleted.ScreenNumber).FirstOrDefault();
+                    if (del != null)
+                    {
+                        _db.SYS_RibbonScreen.DeleteObject(del);
+                    }
+                }
+
+                lstSYS_RibbonScreen.Created.AddRange(lstSYS_RibbonScreen.Updated);
+
+                foreach (SA02100_pgSYS_RibbonScreen_Result curLang in lstSYS_RibbonScreen.Created)
+                {
+                    if (curLang.ScreenNumber.PassNull() == "") continue;
+
+                    var lang = _db.SYS_RibbonScreen.Where(p => p.ScreenNumber.ToLower() == curLang.ScreenNumber.ToLower()).FirstOrDefault();
+
+                    if (lang != null)
+                    {
+                        if (lang.tstamp.ToHex() == curLang.tstamp.ToHex())
+                        {
+                            Update_Language(lang, curLang, false);
+                        }
+                        else
+                        {
+                            throw new MessageException(MessageType.Message, "19");
+                        }
+                    }
+                    else
+                    {
+                        lang = new SYS_RibbonScreen();
+                        Update_Language(lang, curLang, true);
+                        _db.SYS_RibbonScreen.AddObject(lang);
+                    }
+                }
+
+                _db.SaveChanges();
+            
+
+                return Json(new { success = true });
+            }
+            catch (Exception ex)
+            {
+                if (ex is MessageException) return (ex as MessageException).ToMessage();
+                return Json(new { success = false, type = "error", errorMsg = ex.ToString() });
+            }
+        }
+        private void Update_Language(SYS_RibbonScreen t, SA02100_pgSYS_RibbonScreen_Result s, bool isNew)
+        {
+            if (isNew)
+            {
+                t.TabID = s.TabID;
+                t.GroupID = s.GroupID;
+                t.ScreenNumber = s.ScreenNumber;
+
+                t.Crtd_Datetime = DateTime.Now;
+                t.Crtd_Prog = _screenNbr;
+                t.Crtd_User = _userName;
+            }
+            t.LUpd_Datetime = DateTime.Now;
+            t.LUpd_Prog = _screenNbr;
+            t.LUpd_User = _userName;
+        }
+      
+
+
+        
+    }
+}
---
+++ SA/SA02100/SA02100/Scripts/Screen/SA02100.debug.js
@@ -0,0 +1,114 @@
+﻿//// Declare //////////////////////////////////////////////////////////
+
+var keys = ['ScreenNumber', 'TabID', 'GroupID'];
+var fieldsCheckRequire = ["ScreenNumber", "TabID", "GroupID"];
+var fieldsLangCheckRequire = ["ScreenNumber", "TabID", "GroupID"];
+///////////////////////////////////////////////////////////////////////
+//// Store /////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////
+//// Event /////////////////////////////////////////////////////////////
+
+var menuClick = function (command) {
+    switch (command) {
+        case "first":
+            HQ.grid.first(App.grdSYS_RibbonScreen);
+            break;
+        case "prev":
+            HQ.grid.prev(App.grdSYS_RibbonScreen);
+            break;
+        case "next":
+            HQ.grid.next(App.grdSYS_RibbonScreen);
+            break;
+        case "last":
+            HQ.grid.last(App.grdSYS_RibbonScreen);
+            break;
+        case "refresh":
+            App.stoSYS_RibbonScreen.reload();
+            HQ.grid.first(App.grdSYS_RibbonScreen);
+            break;
+        case "new":
+            if (HQ.isInsert) {
+                HQ.grid.insert(App.grdSYS_RibbonScreen);
+            }
+            break;
+        case "delete":
+            if (App.slmSYS_RibbonScreen.selected.items[0] != undefined) {
+                if (HQ.isDelete) {
+                    HQ.message.show(11, '', 'deleteData');
+                }
+            }
+            break;
+        case "save":
+            if (HQ.isUpdate || HQ.isInsert || HQ.isDelete) {
+                if (HQ.store.checkRequirePass(App.stoSYS_RibbonScreen, keys, fieldsCheckRequire, fieldsLangCheckRequire)) {
+                    save();
+                }
+            }
+            break;
+        case "print":
+            break;
+        case "close":
+            if (HQ.store.isChange(App.stoSYS_RibbonScreen)) {
+                HQ.message.show(5, '', 'askClose');
+            } else {
+                HQ.common.close(this);
+            }
+            break;
+    }
+
+};
+var cboTabID_Change = function () {
+    App.slmSYS_RibbonScreen.selected.items[0].set('GroupID', '');
+};
+var grdSYS_RibbonScreen_BeforeEdit = function (editor, e) {
+    if (!HQ.grid.checkBeforeEdit(e, keys)) return false;
+    //nếu cell đang click vào là State thì reload store của cboState
+    if (e.field == "GroupID") {
+        App.cboGroupID.getStore().reload();
+    }
+};
+var grdSYS_RibbonScreen_Edit = function (item, e) {
+    HQ.grid.checkInsertKey(App.grdSYS_RibbonScreen, e, keys);
+};
+var grdSYS_RibbonScreen_ValidateEdit = function (item, e) {
+    return HQ.grid.checkValidateEdit(App.grdSYS_RibbonScreen, e, keys);
+};
+var grdSYS_RibbonScreen_Reject = function (record) {
+    HQ.grid.checkReject(record, App.grdSYS_RibbonScreen);
+};
+/////////////////////////////////////////////////////////////////////////
+//// Process Data ///////////////////////////////////////////////////////
+var save = function () {
+    if (App.frmMain.isValid()) {
+        App.frmMain.submit({
+            waitMsg: HQ.common.getLang("SavingData"),
+            url: 'SA02100/Save',
+            params: {
+                lstSYS_RibbonScreen: HQ.store.getData(App.stoSYS_RibbonScreen)
+            },
+            success: function (msg, data) {
+                HQ.message.show(201405071);
+                menuClick("refresh");
+            },
+            failure: function (msg, data) {
+                HQ.message.process(msg, data, true);
+            }
+        });
+    }
+};
+
+var deleteData = function (item) {
+    if (item == "yes") {
+        App.grdSYS_RibbonScreen.deleteSelected();
+    }
+};
+
+
+/////////////////////////////////////////////////////////////////////////
+//// Other Functions ////////////////////////////////////////////////////
+var askClose = function (item) {
+    if (item == "no" || item == "ok") {
+        HQ.common.close(this);
+    }
+};
+/////////////////////////////////////////////////////////////////////////
