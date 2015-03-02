ef28fe0e11e068e191f70546dec3ad7103aaabc4
phuchd:1 <phuchd:1@hqsoft.tfs.local>
2015-03-02 10:11:39 +0000

    git-tfs-id: [https://hqsofttfs.hqsoft.com.vn/tfs/HQSOFT]$/Cloud/eSky4D;C29766

---
+++ SA/SA02900/SA02900/Controllers/SA02900Controller.cs
@@ -0,0 +1,196 @@
+using HQ.eSkyFramework;
+using Ext.Net;
+using Ext.Net.MVC;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PartialViewResult = System.Web.Mvc.PartialViewResult;
+using System.IO;
+using System.Text;
+namespace SA02900.Controllers
+{
+    [DirectController]
+    [CustomAuthorize]
+    [CheckSessionOut]
+    public class SA02900Controller : Controller
+    {
+        private string _screenNbr = "SA02900";
+        private string _userName = Current.UserName;
+        SA02900Entities _db = Util.CreateObjectContext<SA02900Entities>(false);
+
+        public ActionResult Index()
+        {
+            Util.InitRight(_screenNbr);
+            return View();
+        }
+
+        [OutputCache(Duration = 1000000, VaryByParam = "lang")]
+        public PartialViewResult Body(string lang)
+        {
+            return PartialView();
+        }
+
+        public ActionResult GetTopGrid()
+        {           
+            return this.Store(_db.SA02900_pgSI_ApprovalFlowStatus().ToList());
+        }
+
+        public ActionResult GetBotGrid(string AppFolID, string RoleID, string Status)
+        {
+            return this.Store(_db.SA02900_pgSI_ApprovalFlowHandle(AppFolID, RoleID, Status).ToList());
+        }
+
+        [HttpPost]
+        public ActionResult Save(FormCollection data,string AppFolID, string RoleID, string Status)
+        {
+            try
+            {
+                StoreDataHandler dataHandler = new StoreDataHandler(data["lstTopGrid"]);
+                ChangeRecords<SA02900_pgSI_ApprovalFlowStatus_Result> lstTopGrid = dataHandler.BatchObjectData<SA02900_pgSI_ApprovalFlowStatus_Result>();
+
+                StoreDataHandler dataHandler1 = new StoreDataHandler(data["lstBotGrid"]);
+                ChangeRecords<SA02900_pgSI_ApprovalFlowHandle_Result> lstBotGrid = dataHandler1.BatchObjectData<SA02900_pgSI_ApprovalFlowHandle_Result>();
+                
+                #region Save Top Grid SI_ApprovalFlowStatus
+                foreach (SA02900_pgSI_ApprovalFlowStatus_Result deleted in lstTopGrid.Deleted)
+                {
+                    var del = _db.SI_ApprovalFlowStatus.Where(p => p.AppFolID == deleted.AppFolID && p.RoleID == deleted.RoleID && p.Status == deleted.Status).FirstOrDefault();
+                    if (del != null)
+                    {
+                        _db.SI_ApprovalFlowStatus.DeleteObject(del);
+                    }
+                }
+
+                lstTopGrid.Created.AddRange(lstTopGrid.Updated);
+
+                foreach (SA02900_pgSI_ApprovalFlowStatus_Result curLang in lstTopGrid.Created)
+                {
+                    if (curLang.AppFolID.PassNull() == "" && curLang.RoleID.PassNull() == "" && curLang.Status.PassNull() == "") continue;
+
+                    var lang = _db.SI_ApprovalFlowStatus.Where(p => p.AppFolID == curLang.AppFolID && p.RoleID == curLang.RoleID && p.Status == curLang.Status).FirstOrDefault();
+
+                    if (lang != null)
+                    {
+                        if (lang.tstamp.ToHex() == curLang.tstamp.ToHex())
+                        {
+                            Update_TopGrid(lang, curLang, false);
+                        }
+                        else
+                        {
+                            throw new MessageException(MessageType.Message, "19");
+                        }
+                    }
+                    else
+                    {
+                        lang = new SI_ApprovalFlowStatus();
+                        Update_TopGrid(lang, curLang, true);
+                        _db.SI_ApprovalFlowStatus.AddObject(lang);
+                    }
+                }
+                #endregion
+
+                #region Save Bot Grid SI_ApprovalFlowHandle
+                foreach (SA02900_pgSI_ApprovalFlowHandle_Result deleted in lstBotGrid.Deleted)
+                {
+                    var del = _db.SI_ApprovalFlowHandle.Where(p => p.Handle == deleted.Handle).FirstOrDefault();
+                    if (del != null)
+                    {
+                        _db.SI_ApprovalFlowHandle.DeleteObject(del);
+                    }
+                }
+
+                lstBotGrid.Created.AddRange(lstBotGrid.Updated);
+
+                foreach (SA02900_pgSI_ApprovalFlowHandle_Result curLang in lstBotGrid.Created)
+                {
+                    if (curLang.Handle.PassNull() == "" ) continue;
+
+                    var lang = _db.SI_ApprovalFlowHandle.Where(p => p.Handle == curLang.Handle).FirstOrDefault();
+
+                    if (lang != null)
+                    {
+                        if (lang.tstamp.ToHex() == curLang.tstamp.ToHex())
+                        {
+                            Update_BotGrid(lang, curLang, false);
+                        }
+                        else
+                        {
+                            throw new MessageException(MessageType.Message, "19");
+                        }
+                    }
+                    else
+                    {
+                        lang = new SI_ApprovalFlowHandle();
+                        lang.AppFolID = AppFolID;
+                        lang.RoleID = RoleID;
+                        lang.Status = Status;
+                        Update_BotGrid(lang, curLang, true);
+                        _db.SI_ApprovalFlowHandle.AddObject(lang);
+                    }
+                }
+                #endregion
+
+                _db.SaveChanges();
+                return Json(new { success = true });
+            }
+            catch (Exception ex)
+            {
+                if (ex is MessageException) return (ex as MessageException).ToMessage();
+                return Json(new { success = false, type = "error", errorMsg = ex.ToString() });
+            }
+        }
+
+        private void Update_TopGrid(SI_ApprovalFlowStatus t, SA02900_pgSI_ApprovalFlowStatus_Result s, bool isNew)
+        {
+            if (isNew)
+            {
+                t.AppFolID = s.AppFolID;
+                t.RoleID = s.RoleID;
+                t.Status = s.Status;
+                t.Crtd_Datetime = DateTime.Now;
+                t.Crtd_Prog = _screenNbr;
+                t.Crtd_User = _userName;
+            }
+            t.Descr = s.Descr;
+            t.LangStatus = s.LangStatus;
+
+            t.LUpd_Datetime = DateTime.Now;
+            t.LUpd_Prog = _screenNbr;
+            t.LUpd_User = _userName;
+        }
+
+        private void Update_BotGrid(SI_ApprovalFlowHandle t, SA02900_pgSI_ApprovalFlowHandle_Result s, bool isNew)
+        {
+            if (isNew)
+            {
+                t.Handle = s.Handle;
+                t.Crtd_Datetime = DateTime.Now;
+                t.Crtd_Prog = _screenNbr;
+                t.Crtd_User = _userName;
+            }
+            t.LangHandle = s.LangHandle;
+            t.ToStatus = s.ToStatus;
+            t.ContentApprove = s.ContentApprove;
+            t.MailSubject = s.MailSubject;
+            t.MailApprove = s.MailApprove;
+            t.ProcContent = s.ProcContent;
+            t.MailTo = s.MailTo;
+            t.MailCC = s.MailCC;
+            t.ProcName = s.ProcName;
+            t.Param00 = s.Param00;
+            t.Param01 = s.Param01;
+            t.Param02 = s.Param02;
+            t.Param03 = s.Param03;
+            t.Param04 = s.Param04;
+            t.Param05 = s.Param05;
+
+
+            t.LUpd_Datetime = DateTime.Now;
+            t.LUpd_Prog = _screenNbr;
+            t.LUpd_User = _userName;
+        }
+    }
+}
---
+++ SA/SA02900/SA02900/Scripts/Screen/SA02900.js
@@ -0,0 +1,135 @@
+//// Declare //////////////////////////////////////////////////////////
+
+var keys = ['Code'];
+var fieldsCheckRequire = ["Code", "Lang00", "Lang01"];
+var fieldsLangCheckRequire = ["Code", "Lang00", "Lang01"];
+///////////////////////////////////////////////////////////////////////
+//// Store /////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////
+//// Event /////////////////////////////////////////////////////////////
+
+var menuClick = function (command) {
+    switch (command) {
+        case "first":
+            HQ.grid.first(App.grdSYS_Language);
+            break;
+        case "prev":
+            HQ.grid.prev(App.grdSYS_Language);
+            break;
+        case "next":
+            HQ.grid.next(App.grdSYS_Language);
+            break;
+        case "last":
+            HQ.grid.last(App.grdSYS_Language);
+            break;
+        case "refresh":
+            HQ.isFirstLoad = true;
+            App.stoSYS_Language.reload();
+            break;
+        case "new":
+            if (HQ.isInsert) {
+                HQ.grid.insert(App.grdSYS_Language, keys);
+            }
+            break;
+        case "delete":
+            if (App.slmSYS_Language.selected.items[0] != undefined) {
+                if (HQ.isDelete) {
+                    HQ.message.show(11, '', 'deleteData');
+                }
+            }
+            break;
+        case "save":
+            if (HQ.isUpdate || HQ.isInsert || HQ.isDelete) {
+                if (HQ.store.checkRequirePass(App.stoSYS_Language, keys, fieldsCheckRequire, fieldsLangCheckRequire)) {
+                    save();
+                }
+            }
+            break;
+        case "print":
+            break;
+        case "close":
+            if (HQ.isChange) {
+                HQ.message.show(5, '', 'askClose');
+            } else {
+                HQ.common.close(this);
+            }
+            break;
+    }
+
+};
+var grdSYS_Language_BeforeEdit = function (editor, e) {
+    return HQ.grid.checkBeforeEdit(e, keys);
+};
+var grdSYS_Language_Edit = function (item, e) {
+    HQ.grid.checkInsertKey(App.grdSYS_Language, e, keys);
+};
+var grdSYS_Language_ValidateEdit = function (item, e) {
+    return HQ.grid.checkValidateEdit(App.grdSYS_Language, e, keys);
+};
+var grdSYS_Language_Reject = function (record) {
+    HQ.grid.checkReject(record, App.grdSYS_Language);
+    stoChanged(App.stoSYS_Language);
+};
+/////////////////////////////////////////////////////////////////////////
+//// Process Data ///////////////////////////////////////////////////////
+var save = function () {
+    if (App.frmMain.isValid()) {
+        App.frmMain.submit({
+            waitMsg: HQ.common.getLang("SavingData"),
+            url: 'SA02900/Save',
+            params: {
+                lstSYS_Language: HQ.store.getData(App.stoSYS_Language)
+            },
+            success: function (msg, data) {
+                HQ.message.show(201405071);
+                menuClick("refresh");
+            },
+            failure: function (msg, data) {
+                HQ.message.process(msg, data, true);
+            }
+        });
+    }
+};
+
+var deleteData = function (item) {
+    if (item == "yes") {
+        App.grdSYS_Language.deleteSelected();
+        stoChanged(App.stoSYS_Language);
+    }
+};
+
+
+/////////////////////////////////////////////////////////////////////////
+//// Other Functions ////////////////////////////////////////////////////
+var askClose = function (item) {
+    if (item == "no" || item == "ok") {
+        HQ.common.changeData(false, 'SA02900');//khi dong roi gan lai cho change la false
+        HQ.common.close(this);
+    }
+};
+//load khi giao dien da load xong, gan  HQ.isFirstLoad=true de biet la load lan dau
+var firstLoad = function () {
+    HQ.isFirstLoad = true;
+    App.stoSYS_Language.reload();
+}
+//khi có sự thay đổi thêm xóa sửa trên lưới gọi tới để set * cho header de biết đã có sự thay đổi của grid
+var stoChanged = function (sto) {
+    HQ.isChange = HQ.store.isChange(sto);
+    HQ.common.changeData(HQ.isChange, 'SA02900');
+};
+//load lai trang, kiem tra neu la load lan dau thi them dong moi vao
+var stoLoad = function (sto) {
+    HQ.common.showBusy(false);
+    HQ.isChange = HQ.store.isChange(sto);
+    HQ.common.changeData(HQ.isChange, 'SA02900');
+    if (HQ.isFirstLoad) {
+        if (HQ.isInsert) {
+            HQ.store.insertBlank(sto, keys);
+        }
+        HQ.isFirstLoad = false;
+    }
+};
+//trước khi load trang busy la dang load data
+var stoBeforeLoad = function (sto) {
+    HQ.common.showBusy(true, HQ.common.getLang('loadingdata'));
+};
