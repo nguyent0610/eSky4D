2f42251fb10074049317c3dd9caacbc7423bb33c
phuchd:1 <phuchd:1@hqsoft.tfs.local>
2015-03-09 07:16:55 +0000

    git-tfs-id: [https://hqsofttfs.hqsoft.com.vn/tfs/HQSOFT]$/Cloud/eSky4D;C29945

--- SA/SA00700/SA00700/Properties/AssemblyInfo.cs
+++ SA/SA00700/SA00700/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SA00700")]
-[assembly: AssemblyDescription("Màn hình định nghĩa ngôn ngữ")]
+[assembly: AssemblyDescription("Access Rights")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Han Quang Software Solution")]
 [assembly: AssemblyProduct("HQ Solutions - Cloud Framework")]
