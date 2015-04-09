38648cd83b886ca2e6cdad5eb2027aa890cacddf
phuchd:1 <phuchd:1@hqsoft.tfs.local>
2015-04-09 03:09:50 +0000

    git-tfs-id: [https://hqsofttfs.hqsoft.com.vn/tfs/HQSOFT]$/Cloud/eSky4D;C30667

--- OM/OM22100/OM22100/Controllers/OM22100Controller.cs
+++ OM/OM22100/OM22100/Controllers/OM22100Controller.cs
@@ -58,7 +58,7 @@ namespace OM22100.Controllers
             return View();
         }
         private JsonResult _logMessage;
-        //[OutputCache(Duration = 1000000, VaryByParam = "lang")]
+        [OutputCache(Duration = 1000000, VaryByParam = "lang")]
         public PartialViewResult Body(string lang)
         {
             return PartialView();
