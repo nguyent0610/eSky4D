27a445aaeb39b5617e96a5b3f25445e08020511d
phuchd:1 <phuchd:1@hqsoft.tfs.local>
2015-04-13 11:05:00 +0000

    git-tfs-id: [https://hqsofttfs.hqsoft.com.vn/tfs/HQSOFT]$/Cloud/eSky4D;C30801

--- IN/IN22002/IN22002/Properties/AssemblyInfo.cs
+++ IN/IN22002/IN22002/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("IN22002")]
-[assembly: AssemblyDescription("Statement Cycle")]
+[assembly: AssemblyDescription("IN22002")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Han Quang Software Solution")]
 [assembly: AssemblyProduct("HQ Solutions - Cloud Framework")]
