f84d904ff04193231d8a5687a655f28dafcdec10
phuchd:1 <phuchd:1@hqsoft.tfs.local>
2015-04-20 08:47:11 +0000

    git-tfs-id: [https://hqsofttfs.hqsoft.com.vn/tfs/HQSOFT]$/Cloud/eSky4D;C30947

--- SA/SA00300/SA00300/Controllers/SA00300Controller.cs
+++ SA/SA00300/SA00300/Controllers/SA00300Controller.cs
@@ -48,7 +48,7 @@ namespace SA00300.Controllers
             return View();
         }
 
-        //[OutputCache(Duration = 1000000, VaryByParam = "lang")]
+        [OutputCache(Duration = 1000000, VaryByParam = "lang")]
         public PartialViewResult Body(string lang)
         {
             return PartialView();
