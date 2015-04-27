da1b6b5b1f00055547f99e0f577523e4e0017ff8
phuchd:1 <phuchd:1@hqsoft.tfs.local>
2015-04-27 01:57:16 +0000

    git-tfs-id: [https://hqsofttfs.hqsoft.com.vn/tfs/HQSOFT]$/Cloud/eSky4D;C31070

---
+++ SI/SI21900/SI21900/Scripts/Screen/SI21900.debug.js
@@ -0,0 +1,138 @@
+//// Declare ///////////////////////////////////////////////////////////
+var keys = ['Territory'];
+var fieldsCheckRequire = ["Territory"];
+var fieldsLangCheckRequire = ["Territory"];
+////////////////////////////////////////////////////////////////////////
+//// Store /////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////
+//// Event /////////////////////////////////////////////////////////////
+
+var menuClick = function (command) {
+    switch (command) {
+        case "first":
+            HQ.grid.first(App.grdTerritory);
+            break;
+        case "prev":
+            HQ.grid.prev(App.grdTerritory);
+            break;
+        case "next":
+            HQ.grid.next(App.grdTerritory);
+            break;
+        case "last":
+            HQ.grid.last(App.grdTerritory);
+            break;
+        case "refresh":
+            if (HQ.isChange) {
+                HQ.message.show(20150303, '', 'refresh');
+            }
+            else {
+                HQ.isChange = false;
+                HQ.isFirstLoad = true;
+                App.stoTerritory.reload();
+            }
+            break;
+        case "new":
+            if (HQ.isInsert) {
+                HQ.grid.insert(App.grdTerritory, keys);
+            }
+            break;
+        case "delete":
+            if (App.slmTerritory.selected.items[0] != undefined) {
+                var rowindex = HQ.grid.indexSelect(App.grdTerritory);
+                if (rowindex != '')
+                    HQ.message.show(2015020807, [HQ.grid.indexSelect(App.grdTerritory), ''], 'deleteData', true)
+            }
+            break;
+        case "save":
+            if (HQ.isUpdate || HQ.isInsert || HQ.isDelete) {
+                if (HQ.store.checkRequirePass(App.stoTerritory, keys, fieldsCheckRequire, fieldsLangCheckRequire)) {
+                    save();
+                }
+            }
+            break;
+        case "print":
+            break;
+        case "close":
+            HQ.common.close(this);
+            break;
+    }
+
+};
+var grdTerritory_BeforeEdit = function (editor, e) {
+    return HQ.grid.checkBeforeEdit(e, keys);
+};
+var grdTerritory_Edit = function (item, e) {
+    HQ.grid.checkInsertKey(App.grdTerritory, e, keys);
+};
+var grdTerritory_ValidateEdit = function (item, e) {
+    return HQ.grid.checkValidateEdit(App.grdTerritory, e, keys);
+};
+var grdTerritory_Reject = function (record) {
+    HQ.grid.checkReject(record, App.grdTerritory);
+    stoChanged(App.stoTerritory);
+};
+/////////////////////////////////////////////////////////////////////////
+//// Process Data ///////////////////////////////////////////////////////
+var save = function () {
+    if (App.frmMain.isValid()) {
+        App.frmMain.submit({
+            waitMsg: HQ.common.getLang("SavingData"),
+            url: 'SI21900/Save',
+            params: {
+                lstTerritory: HQ.store.getData(App.stoTerritory)
+            },
+            success: function (msg, data) {
+                HQ.message.show(201405071);
+                HQ.isChange = false;
+                menuClick("refresh");
+            },
+            failure: function (msg, data) {
+                HQ.message.process(msg, data, true);
+            }
+        });
+    }
+};
+
+var deleteData = function (item) {
+    if (item == "yes") {
+        App.grdTerritory.deleteSelected();
+        stoChanged(App.stoTerritory);
+    }
+};
+
+//load khi giao dien da load xong, gan  HQ.isFirstLoad=true de biet la load lan dau
+var firstLoad = function () {
+    HQ.isFirstLoad = true;
+    App.stoTerritory.reload();
+}
+//khi có sự thay đổi thêm xóa sửa trên lưới gọi tới để set * cho header de biết đã có sự thay đổi của grid
+var stoChanged = function (sto) {
+    HQ.isChange = HQ.store.isChange(sto);
+    HQ.common.changeData(HQ.isChange, 'SI21900');
+};
+//load lai trang, kiem tra neu la load lan dau thi them dong moi vao
+var stoLoad = function (sto) {
+    HQ.common.showBusy(false);
+    HQ.isChange = HQ.store.isChange(sto);
+    HQ.common.changeData(HQ.isChange, 'SI21900');
+    if (HQ.isFirstLoad) {
+        if (HQ.isInsert) {
+            HQ.store.insertBlank(sto, keys);
+        }
+        HQ.isFirstLoad = false;
+    }
+};
+//trước khi load trang busy la dang load data
+var stoBeforeLoad = function (sto) {
+    HQ.common.showBusy(true, HQ.common.getLang('loadingdata'));
+};
+
+/////////////////////////////////////////////////////////////////////////
+//// Other Functions ////////////////////////////////////////////////////
+function refresh(item) {
+    if (item == 'yes') {
+        HQ.isChange = false;
+        HQ.isFirstLoad = true;
+        App.stoTerritory.reload();
+    }
+};
