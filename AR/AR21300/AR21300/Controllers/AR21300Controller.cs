cae80c9ca8b1f78a7738202f77bd2dd386d9e26b
phuchd:1 <phuchd:1@hqsoft.tfs.local>
2015-05-07 02:21:14 +0000

    git-tfs-id: [https://hqsofttfs.hqsoft.com.vn/tfs/HQSOFT]$/Cloud/eSky4D;C31170

--- AR/AR21300/AR21300/Controllers/AR21300Controller.cs
+++ AR/AR21300/AR21300/Controllers/AR21300Controller.cs
@@ -19,7 +19,9 @@ namespace AR21300.Controllers
     public class AR21300Controller : Controller
     {
         private string _screenNbr = "AR21300";
+        private string _userName = Current.UserName;
         AR21300Entities _db = Util.CreateObjectContext<AR21300Entities>(false);
+
         public ActionResult Index()
         {
             Util.InitRight(_screenNbr);
@@ -29,117 +31,83 @@ namespace AR21300.Controllers
         [OutputCache(Duration = 1000000, VaryByParam = "lang")]
         public PartialViewResult Body(string lang)
         {
-            return PartialView(_db.AR21300_pgLoadArea().ToList());
+            return PartialView();
         }
+
         public ActionResult GetData()
         {
             return this.Store(_db.AR21300_pgLoadArea().ToList());
         }
-      
-        [HttpPost]      
+
+        [HttpPost]
         public ActionResult Save(FormCollection data)
         {
             try
             {
-            StoreDataHandler dataHandler = new StoreDataHandler(data["lstAR_Area"]);
-            ChangeRecords<AR_Area> lstAR_Area = dataHandler.BatchObjectData<AR_Area>();
-            foreach (AR_Area deleted in lstAR_Area.Deleted)
-            {
-                var del = _db.AR_Area.Where(p => p.Area == deleted.Area).FirstOrDefault();
-                if (del != null)
+                StoreDataHandler dataHandler = new StoreDataHandler(data["lstAR_Area"]);
+                ChangeRecords<AR21300_pgLoadArea_Result> lstAR_Area = dataHandler.BatchObjectData<AR21300_pgLoadArea_Result>();
+                foreach (AR21300_pgLoadArea_Result deleted in lstAR_Area.Deleted)
                 {
-                    _db.AR_Area.DeleteObject(del);
-                }
-              
-            }
-            foreach (AR_Area created in lstAR_Area.Created)
-            {
-                if (created.Area == "") continue;
-                var record = _db.AR_Area.Where(p => p.Area == created.Area).FirstOrDefault();
-
-                if (created.tstamp.ToHex() == "")
-                {
-                    if (record == null)
+                    var del = _db.AR_Area.Where(p => p.Area == deleted.Area).FirstOrDefault();
+                    if (del != null)
                     {
-                        record = new AR_Area();
-                        record.Area = created.Area;
-                        record.Crtd_Datetime = DateTime.Now;
-                        record.Crtd_Prog = _screenNbr;
-                        record.Crtd_User = Current.UserName;
-                        UpdatingAR_Area(created, ref record);
-                        _db.AR_Area.AddObject(record);
+                        _db.AR_Area.DeleteObject(del);
+                    }
+                }
+
+                lstAR_Area.Created.AddRange(lstAR_Area.Updated);
+
+                foreach (AR21300_pgLoadArea_Result curLang in lstAR_Area.Created)
+                {
+                    if (curLang.Area.PassNull() == "") continue;
+
+                    var lang = _db.AR_Area.Where(p => p.Area.ToLower() == curLang.Area.ToLower()).FirstOrDefault();
+
+                    if (lang != null)
+                    {
+                        if (lang.tstamp.ToHex() == curLang.tstamp.ToHex())
+                        {
+                            Update(lang, curLang, false);
+                        }
+                        else
+                        {
+                            throw new MessageException(MessageType.Message, "19");
+                        }
                     }
                     else
                     {
-                        Message.Show(2000, new string[] { Util.GetLang("Area"), record.Area.ToString() }, null);
-                        return this.Direct();
-                        //tra ve loi da ton tai ma ngon ngu nay ko the them
+                        lang = new AR_Area();
+                        Update(lang, curLang, true);
+                        _db.AR_Area.AddObject(lang);
                     }
                 }
-                else
-                {
-                    Message.Show(2000, new string[] { Util.GetLang("Area"), record.Area.ToString() }, null);
-                    return this.Direct();
-                    //tra ve loi da ton tai ma ngon ngu nay ko the them
-                }
 
-
-            }
-
-            
-
-            foreach (AR_Area updated in lstAR_Area.Updated)
-            {
-                var record = _db.AR_Area.Where(p => p.Area == updated.Area).FirstOrDefault();
-                                
-                if (record != null)
-                {
-                    UpdatingAR_Area(updated, ref record);
-                }
-                else
-                {
-                    record = new AR_Area();
-                    record.Area = updated.Area;
-                    record.Crtd_Datetime = DateTime.Now;
-                    record.Crtd_Prog = _screenNbr;
-                    record.Crtd_User = Current.UserName;                
-                    UpdatingAR_Area(updated, ref record);
-                    _db.AR_Area.AddObject(record);
-                }
-                
-
-            }
-            _db.SaveChanges();
-
-            return Json(new { success = true });
-              
+                _db.SaveChanges();
+                return Json(new { success = true });
             }
             catch (Exception ex)
             {
-
-                return Json(new { success = false, errorMsg = ex.ToString()});
-                
-              
+                if (ex is MessageException) return (ex as MessageException).ToMessage();
+                return Json(new { success = false, type = "error", errorMsg = ex.ToString() });
             }
         }
 
-        private void UpdatingAR_Area(AR_Area s, ref AR_Area d)
+
+        private void Update(AR_Area t, AR21300_pgLoadArea_Result s, bool isNew)
         {
-            d.Descr = s.Descr;                   
-            d.LUpd_Datetime = DateTime.Now;
-            d.LUpd_Prog = _screenNbr;
-            d.LUpd_User = Current.UserName;
+            if (isNew)
+            {
+                t.Area = s.Area;
+                t.Crtd_Datetime = DateTime.Now;
+                t.Crtd_Prog = _screenNbr;
+                t.Crtd_User = _userName;
+            }
+            t.Descr = s.Descr;
+
+            t.LUpd_Datetime = DateTime.Now;
+            t.LUpd_Prog = _screenNbr;
+            t.LUpd_User = _userName;
         }
 
-        [DirectMethod]
-        public ActionResult GetDataGrid()
-        {
-            var lst = _db.AR21300_pgLoadArea().ToList();
-
-            this.GetCmp<TextField>("stoArea").Data = lst;
-              
-
-            return this.Direct(lst);
-        }
     }
 }
