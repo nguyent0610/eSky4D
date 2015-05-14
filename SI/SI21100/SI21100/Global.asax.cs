17230714664cf1ad7db516f2db224eaf616a0788
phuchd:1 <phuchd:1@hqsoft.tfs.local>
2015-05-14 08:59:41 +0000

    git-tfs-id: [https://hqsofttfs.hqsoft.com.vn/tfs/HQSOFT]$/Cloud/eSky4D;C31339

--- SI/SI21100/SI21100/Global.asax.cs
+++ SI/SI21100/SI21100/Global.asax.cs
@@ -48,8 +48,8 @@ namespace SI21100
             Session["DBApp"] = Current.DBApp = "eSky4DApp";
             Session["UserName"] = Current.UserName = "admin";
             Session["CpnyID"] = Current.CpnyID = "18510580";
-            Session["Language"] = Current.Language = "en";
-            Session["LangID"] = 0;
+            Session["Language"] = Current.Language = "vi";
+            Session["LangID"] = 1;
         }
     }
 }
