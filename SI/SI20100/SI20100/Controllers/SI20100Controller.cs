3a896c73bc0b7139f8c9f0850b4ec206598e0038
phuchd:1 <phuchd:1@hqsoft.tfs.local>
2015-05-14 08:01:08 +0000

    git-tfs-id: [https://hqsofttfs.hqsoft.com.vn/tfs/HQSOFT]$/Cloud/eSky4D;C31332

--- SI/SI20100/SI20100/Controllers/SI20100Controller.cs
+++ SI/SI20100/SI20100/Controllers/SI20100Controller.cs
@@ -27,7 +27,7 @@ namespace SI20100.Controllers
             return View();
         }
 
-        [OutputCache(Duration = 1000000, VaryByParam = "lang")]
+        //[OutputCache(Duration = 1000000, VaryByParam = "lang")]
         public PartialViewResult Body(string lang)
         {
             return PartialView();
