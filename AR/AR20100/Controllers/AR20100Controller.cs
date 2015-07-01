4ffcf3e3e7fd13f9bf634119e471b1135fea1d7c
phuchd:1 <phuchd:1@hqsoft.tfs.local>
2015-07-01 01:39:59 +0000

    git-tfs-id: [https://hqsofttfs.hqsoft.com.vn/tfs/HQSOFT]$/Cloud/eSky4D;C32238

--- AR/AR20100/Controllers/AR20100Controller.cs
+++ AR/AR20100/Controllers/AR20100Controller.cs
@@ -29,7 +29,7 @@ namespace AR20100.Controllers
             return View();
         }
 
-        [OutputCache(Duration = 1000000, VaryByParam = "lang")]
+        //[OutputCache(Duration = 1000000, VaryByParam = "lang")]
         public PartialViewResult Body(string lang)
         {
             return PartialView();
