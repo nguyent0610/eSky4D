8fee41c29488bdb7d9e3644e48acf84573ba7fb6
trunght:1 <trunght:1@hqsoft.tfs.local>
2015-07-29 03:57:15 +0000

    git-tfs-id: [https://hqsofttfs.hqsoft.com.vn/tfs/HQSOFT]$/Cloud/eSky4D;C32840

--- OM/OM40600/OM40600/Scripts/Screen/OM40600.js
+++ OM/OM40600/OM40600/Scripts/Screen/OM40600.js
@@ -1,74 +1,14 @@
-//// Declare //////////////////////////////////////////////////////////
 
-var keys = ['CountryID'];
-var fieldsCheckRequire = ["CountryID"];
-var fieldsLangCheckRequire = ["CountryID"];
-///////////////////////////////////////////////////////////////////////
-//// Store /////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////
-//// Event /////////////////////////////////////////////////////////////
-
-var menuClick = function (command) {
-    switch (command) {
-        case "first":
-
-            HQ.grid.first(App.grdDetail);
-            break;
-        case "prev":
-            HQ.grid.prev(App.grdDetail);
-            break;
-        case "next":
-            HQ.grid.next(App.grdDetail);
-            break;
-        case "last":
-            HQ.grid.last(App.grdDetail);
-            break;
-        case "refresh":
-            HQ.isFirstLoad = true;
-            App.stoCountry.reload();
-            break;
-        case "new":
-            if (HQ.isInsert) {
-                HQ.grid.insert(App.grdDetail, keys);
-            }
-            break;
-        case "delete":
-            if (App.slmCountry.selected.items[0] != undefined) {
-                if (HQ.isDelete) {
-                    HQ.message.show(11, '', 'deleteData');
-                }
-            }
-            break;
-        case "save":
-            if (HQ.isUpdate || HQ.isInsert || HQ.isDelete) {
-                if (HQ.store.checkRequirePass(App.stoCountry, keys, fieldsCheckRequire, fieldsLangCheckRequire)) {
-                    save();
-                }
-            }
-            break;
-        case "print":
-            break;
-        case "close":
-            if (HQ.isChange) {
-                HQ.message.show(5, '', 'askClose');
-            } else {
-                HQ.common.close(this);
-            }
-            break;
-    }
-
-};
-
-var chkSelectHeader_change= function (chk, newValue, oldValue, eOpts) {
-    App.stoDetail.data.each(function (record) {                    
-        record.data.Selected = chk.value;           
+var chkSelectHeader_change = function (chk, newValue, oldValue, eOpts) {
+    App.stoDetail.data.each(function (record) {
+        record.data.Selected = chk.value;
     });
     App.grdDetail.view.refresh();
 }
-var dtpFromDate_change= function (dtp, newValue, oldValue, eOpts) {
+var dtpFromDate_change = function (dtp, newValue, oldValue, eOpts) {
     App.dtpToDate.setMinValue(newValue);
     App.dtpToDate.validate();
-}    
+}
 var cboBranchID_Change = function (item, newValue, oldValue) {
     App.cboPJPID.setValue('');
     App.cboRoute.setValue('');
@@ -78,7 +18,7 @@ var cboBranchID_Change = function (item, newValue, oldValue) {
         App.cboPJPID.getStore().reload();
         App.cboSlsperID.getStore().reload();
         App.cboCustID.getStore().reload();
-    }  
+    }
 };
 var cboBranchID_Select = function (item, newValue, oldValue) {
     if (item.hasFocus) {
@@ -129,7 +69,7 @@ var btnLoad_click = function () {
     HQ.routeID = '';
     if (App.cboBranchID.getValue().join(',').length == 0) {
         App.cboBranchID.getStore().each(function (item) {
-            HQ.branchID += item.data.BranchID+',';
+            HQ.branchID += item.data.BranchID + ',';
         });
     }
     else HQ.branchID = App.cboBranchID.getValue().join(',');
@@ -140,17 +80,17 @@ var btnLoad_click = function () {
     else HQ.pJPID = App.cboPJPID.getValue().join(',');
 
     if (App.cboRoute.getValue().join(',').length == 0) {
-            HQ.routeID = '%';       
+        HQ.routeID = '%';
     }
     else HQ.routeID = App.cboRoute.getValue().join(',');
 
     if (App.cboSlsperID.getValue().join(',').length == 0) {
-            HQ.slsperID = '%';        
+        HQ.slsperID = '%';
     }
     else HQ.slsperID = App.cboSlsperID.getValue().join(',');
 
     if (App.cboCustID.getValue().join(',').length == 0) {
-        HQ.custID = '%';       
+        HQ.custID = '%';
     }
     else HQ.custID = App.cboCustID.getValue().join(',');
 
@@ -161,18 +101,12 @@ var btnLoad_click = function () {
 
 /////////////////////////////////////////////////////////////////////////
 //// Other Functions ////////////////////////////////////////////////////
-var askClose = function (item) {
-    if (item == "no" || item == "ok") {
-        HQ.common.changeData(false, 'OM40600');//khi dong roi gan lai cho change la false
-        HQ.common.close(this);
-    }
-};
 //load khi giao dien da load xong, gan  HQ.isFirstLoad=true de biet la load lan dau
 var firstLoad = function () {
     HQ.isFirstLoad = true;
     App.dtpFromDate.setValue(HQ.bussinessDate);
     App.dtpToDate.setValue(HQ.bussinessDate);
-   
+
 }
 //khi có sự thay đổi thêm xóa sửa trên lưới gọi tới để set * cho header de biết đã có sự thay đổi của grid
 var stoChanged = function (sto) {
@@ -184,12 +118,7 @@ var stoLoad = function (sto) {
     HQ.common.showBusy(false);
     HQ.isChange = HQ.store.isChange(sto);
     HQ.common.changeData(HQ.isChange, 'OM40600');
-    if (HQ.isFirstLoad) {
-        if (HQ.isInsert) {
-            HQ.store.insertBlank(sto, keys);
-        }
-        HQ.isFirstLoad = false;
-    }
+
 };
 //trước khi load trang busy la dang load data
 var stoBeforeLoad = function (sto) {
