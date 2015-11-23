ca4b181e3f76331c609c9623efc9bb04668a2f30
trunght:1 <trunght:1@hqsoft.tfs.local>
2015-11-23 03:16:45 +0000

    them field lastslsid
    git-tfs-id: [https://hqsofttfs.hqsoft.com.vn/tfs/HQSOFT]$/Cloud/eSky4D;C35148

--- AR/AR00000/Controllers/AR00000Controller.cs
+++ AR/AR00000/Controllers/AR00000Controller.cs
@@ -1,45 +1,41 @@
-using System.Web.Mvc;
+using HQ.eSkyFramework;
 using Ext.Net;
 using Ext.Net.MVC;
-using eBiz4DWebFrame;
-using eBiz4DWebSys;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
 using System;
-using System.Data.Metadata.Edm;
-using System.Security.Cryptography;
-using System.Text;
-using HQSendMailApprove;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
 using PartialViewResult = System.Web.Mvc.PartialViewResult;
+using System.IO;
+using System.Text;
 namespace AR00000.Controllers
 {
     [DirectController]
+    [CustomAuthorize]
+    [CheckSessionOut]
     public class AR00000Controller : Controller
     {
-        private string _screenName = "AR00000";
-        
+        private string _screenNbr = "AR00000";
+        private string _userName = Current.UserName;
+
         AR00000Entities _db = Util.CreateObjectContext<AR00000Entities>(false);
-        eBiz4DWebSysEntities _sys = Util.CreateObjectContext<eBiz4DWebSysEntities>(true);
 
-        string test = Current.CpnyID;
-
-        
-
-        //
-        // GET: /AR00000/
         public ActionResult Index()
         {
+            
+            Util.InitRight(_screenNbr);
             return View();
         }
 
-        [OutputCache(Duration = 1000000, VaryByParam = "none")]
-        public PartialViewResult Body()
+        [OutputCache(Duration = 1000000, VaryByParam = "lang")]
+        public PartialViewResult Body(string lang)
         {
             return PartialView();
         }
-        // Get collection of Sales Person in a branch for binding data (Ajax)
+
+     
         public ActionResult GetAR00000Header(string branchId, string setupID)
         {
             var setupData = _db.AR_Setup.FirstOrDefault(p => p.BranchID == branchId && p.SetupId == setupID);
@@ -49,141 +45,93 @@ namespace AR00000.Controllers
         [HttpPost]
         public ActionResult Save(FormCollection data)
         {
-            // Get params from data that's sent from client (Ajax)
-            
-            string branchId = Current.CpnyID;
-            //string dfltBankAcct = data["cboBankAcct"];
-            //string classID = data["cboClassID"];
-            //string lastBatNbr = data["txtLastBatNbr"];
-            //string lastRefNbr = data["txtLastRefNbr"];
-            //string lastPaymentNbr = data["txtLastPaymentNbr"];
-            //string preFixBat = data["txtPreFixBat"];
-            //string tranDescDef = data["cboTranDescDef"];
-            //string terms = data["cboTermsID"];
-            
-
-            StoreDataHandler dataHandler = new StoreDataHandler(data["lstAR00000Header"]);
-            
-
-            ChangeRecords<AR_Setup> lstAR00000Header = dataHandler.BatchObjectData<AR_Setup>();
-
-            foreach (AR_Setup updatedSlsperson in lstAR00000Header.Updated)
+            string branchId = Current.CpnyID;         
+            try
             {
-                // Get the image path
 
+                StoreDataHandler dataHandler = new StoreDataHandler(data["lstAR00000Header"]);
+                ChangeRecords<AR_Setup> lstSetup = dataHandler.BatchObjectData<AR_Setup>();
+                
 
-                var objHeader = _db.AR_Setup.FirstOrDefault(p => p.BranchID == branchId && p.SetupId == "AR");
-                if (objHeader != null)
+                lstSetup.Created.AddRange(lstSetup.Updated);
+
+                foreach (AR_Setup curSetup in lstSetup.Created)
                 {
-                    //updating
-                    
-                    if(objHeader.tstamp.ToHex() != updatedSlsperson.tstamp.ToHex())
+                    if (curSetup.BranchID.PassNull() == "") continue;
+
+                    var Setup = _db.AR_Setup.FirstOrDefault(p => p.BranchID == branchId && p.SetupId == "AR");
+
+                    if (Setup != null)
                     {
-                        return Json(new { success = false, code = "19" }, JsonRequestBehavior.AllowGet);
-                    }else
+                        if (Setup.tstamp.ToHex() == curSetup.tstamp.ToHex())
+                        {
+                            Update_Setup(Setup, curSetup, false);
+                        }
+                        else
+                        {
+                            throw new MessageException(MessageType.Message, "19");
+                        }
+                    }
+                    else
                     {
-                        UpdatingHeader(updatedSlsperson, ref objHeader);
+                        Setup = new AR_Setup();
+                        Update_Setup(Setup, curSetup, true);
+                        _db.AR_Setup.AddObject(Setup);
                     }
                 }
-                
-                
-                    
-                    
-                
 
-                // If there is a change in handling status (keepStatus is False),
-                // add a new pending task with an approved handle.
-                
-                    _db.SaveChanges();
-                
-                // ===============================================================
-
-                // Get out of the loop (only update the first data)
-                break;
+                _db.SaveChanges();
+                return Json(new { success = true });
             }
-            foreach (AR_Setup createdAR00000 in lstAR00000Header.Created)
+            catch (Exception ex)
             {
-                var objHeader = _db.AR_Setup.FirstOrDefault(p => p.BranchID == branchId && p.SetupId == "AR");
-                if (objHeader == null)
-                {
-                    objHeader = new AR_Setup();
-                    
-                    objHeader.BranchID = branchId;
-                    objHeader.SetupId = "AR";
-                    objHeader.LastBatNbr = createdAR00000.LastBatNbr; // ???
-                    objHeader.AutoCustID = createdAR00000.AutoCustID;
-                    objHeader.LastRefNbr = createdAR00000.LastRefNbr;
-                    objHeader.AutoSlsperID = createdAR00000.AutoSlsperID;
-                    objHeader.LastReceiptNbr = createdAR00000.LastReceiptNbr;
-                    objHeader.HiddenHierarchy = createdAR00000.HiddenHierarchy;
-                    objHeader.LastCustID = createdAR00000.LastCustID;
-                    objHeader.DfltShipViaID = createdAR00000.DfltShipViaID;
-                    objHeader.PrefixBat = createdAR00000.PrefixBat;
-                    objHeader.TranDescDflt = createdAR00000.TranDescDflt;
-                    objHeader.AddressLevel = createdAR00000.AddressLevel;
-                    objHeader.DfltBankAcct = createdAR00000.DfltBankAcct;
-
-
-                    objHeader.Crtd_DateTime = DateTime.Now;
-                    objHeader.Crtd_Prog = _screenName;
-                    objHeader.Crtd_User = Current.UserName;
-                    objHeader.tstamp = new byte[0];
-                    UpdatingHeader(createdAR00000, ref objHeader);
-                    _db.AR_Setup.AddObject(objHeader);
-                    _db.SaveChanges();
-                    
-                }
+                if (ex is MessageException) return (ex as MessageException).ToMessage();
+                return Json(new { success = false, type = "error", errorMsg = ex.ToString() });
             }
-            //_db.SaveChanges();
-
-            return Json(new { success = true });
         }
-
-        // Delete a Sales Person and his picture file
         [DirectMethod]
         public ActionResult Delete(string branchId)
         {
             branchId = Current.CpnyID;
             var cpny = _db.AR_Setup.FirstOrDefault(p => p.BranchID == branchId);
-            if (cpny != null )
+            if (cpny != null)
             {
                 _db.AR_Setup.DeleteObject(cpny);
-               
-            }
 
+            }   
             _db.SaveChanges();
             return this.Direct();
         }
-
-        private void UpdatingHeader(AR_Setup s, ref AR_Setup d)
+        private void Update_Setup(AR_Setup s, AR_Setup d, bool isNew)
         {
+            if (isNew)
+            {
+                s.BranchID = d.BranchID;
+                s.SetupId = "AR";
+                s.Crtd_DateTime = DateTime.Now;
+                s.Crtd_Prog = _screenNbr;
+                s.Crtd_User = _userName;
+            }
 
-            d.LastBatNbr = s.LastBatNbr;
-            d.AutoCustID = s.AutoCustID == null ? false : s.AutoCustID;
-            d.LastRefNbr = s.LastRefNbr;
-            d.AutoSlsperID = s.AutoSlsperID == null ? false : s.AutoSlsperID;
-            d.LastReceiptNbr = s.LastReceiptNbr;
-            d.HiddenHierarchy = s.AutoSlsperID == null ? false : s.HiddenHierarchy;
+            s.LastBatNbr =d.LastBatNbr;
+            s.AutoCustID = d.AutoCustID == null ? false : d.AutoCustID;
+            s.LastRefNbr = d.LastRefNbr;
+            s.AutoSlsperID = d.AutoSlsperID == null ? false : d.AutoSlsperID;
+            s.LastReceiptNbr =d.LastReceiptNbr;
+            s.HiddenHierarchy = d.AutoSlsperID == null ? false : d.HiddenHierarchy;
 
+            s.LastCustID = d.LastCustID;
+            s.LastSlsID = d.LastSlsID;
+            s.DfltShipViaID = d.DfltShipViaID;
+            s.PrefixBat = d.PrefixBat;
+            s.TranDescDflt = d.TranDescDflt;
+            s.AddressLevel = d.AddressLevel;
+            s.DfltBankAcct = d.DfltBankAcct;
 
-            d.LastCustID = s.LastCustID;
-            d.DfltShipViaID = s.DfltShipViaID;
-            d.PrefixBat = s.PrefixBat;
-            d.TranDescDflt = s.TranDescDflt;
-            d.AddressLevel = s.AddressLevel;
-            d.DfltBankAcct = s.DfltBankAcct;
-                 
-
-            d.LUpd_DateTime = DateTime.Now;
-            d.LUpd_Prog = _screenName;
-            d.LUpd_User = Current.UserName;
+            s.LUpd_DateTime = DateTime.Now;
+            s.LUpd_Prog = _screenNbr;
+            s.LUpd_User = _userName;
         }
-
         
-
-        // Upload and preview the image.
-       
-
-       
     }
 }
--- AR/AR00000/Global.asax.cs
+++ AR/AR00000/Global.asax.cs
@@ -1,13 +1,13 @@
-using eBiz4DWebFrame;
 using System;
 using System.Collections.Generic;
-using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using HQ.eSkyFramework;
+using System.Configuration;
 
 namespace AR00000
 {
@@ -24,20 +24,23 @@ namespace AR00000
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
-        }
 
+
+        }
         protected void Session_Start(object sender, EventArgs e)
         {
-            Current.Server = "MARSSVR\\SQL2012";// ConfigurationManager.AppSettings["Server"].ToString();
-            Current.DBSys = "eBiz4DCloudSysMVC";// ConfigurationManager.AppSettings["DBSys"].ToString();
+
+            Current.Authorize = false;
+            Current.Server = "MARSSVR\\SQL2012";
+            Current.DBSys = "eSky4DSys";
             AccessRight acc = new AccessRight();
             acc.Delete = true;
             acc.Insert = true;
             acc.Update = true;
             Session["AR00000"] = acc;
-            Session["DBApp"] = Current.DBApp = "eBiz4DCloudAppMVC";
+            Session["DBApp"] = Current.DBApp = "eSky4DApp";
             Session["UserName"] = Current.UserName = "admin";
-            Session["CpnyID"] = Current.CpnyID = "jjj";
+            Session["CpnyID"] = Current.CpnyID = "HQHD3110";
             Session["Language"] = Current.Language = "vi";
             Session["LangID"] = 1;
         }
