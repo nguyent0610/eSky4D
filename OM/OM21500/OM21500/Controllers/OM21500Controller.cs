926f09071485d9f7c88928a3a77298625b295d46
trunght:1 <trunght:1@hqsoft.tfs.local>
2016-04-08 07:25:23 +0000

    FORMAT DATE THEO CHUAN
    git-tfs-id: [https://hqsofttfs.hqsoft.com.vn/tfs/HQSOFT]$/Cloud/eSky4D;C38256

--- OM/OM21500/OM21500/Controllers/OM21500Controller.cs
+++ OM/OM21500/OM21500/Controllers/OM21500Controller.cs
@@ -26,7 +26,7 @@ namespace OM21500.Controllers
             return View();
         }
 
-       // [OutputCache(Duration = 1000000, VaryByParam = "lang")]
+        [OutputCache(Duration = 1000000, VaryByParam = "lang")]
         public PartialViewResult Body(string lang)
         {
             return PartialView();
