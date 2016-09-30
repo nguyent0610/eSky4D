66b42320b8e3dff989626b6a1f9a4f1e4d6dba3b
trunght:2 <trunght:2@hqsoft.tfs.local>
2016-09-30 04:03:34 +0000

    debug cho JFV
    git-tfs-id: [https://hqsofttfs.hqsoft.com.vn/tfs/HQSOFT]$/Cloud/eSky4D;C42485

--- OM/OM10100/Global.asax.cs
+++ OM/OM10100/Global.asax.cs
@@ -38,17 +38,17 @@ namespace OM10100
             //Session["LangID"] = 1;
 
             Current.Authorize = false;
-            Current.Server = "EARTHSVR\\SQL2012";// ConfigurationManager.AppSettings["Server"].ToString();
-            Current.DBSys = "ND_eSky4DSys";//ConfigurationManager.AppSettings["DBSys"].ToString();
+            Current.Server = "HQDBDG";// ConfigurationManager.AppSettings["Server"].ToString();
+            Current.DBSys = "eBiz4DCloudSysJFV";//ConfigurationManager.AppSettings["DBSys"].ToString();
             Current.FormatDate = "dd.MM.yyyy";
             AccessRight acc = new AccessRight();
             acc.Delete = true;
             acc.Insert = true;
             acc.Update = true;
             Session["OM10100"] = acc;
-            Session["DBApp"] = Current.DBApp = "ND_eSky4DApp";// "eBiz4DWebApp";
-            Session["UserName"] = Current.UserName = "admin";
-            Session["CpnyID"] = Current.CpnyID = "040100071";
+            Session["DBApp"] = Current.DBApp = "eBiz4DCloudAppJFV";// "eBiz4DWebApp";
+            Session["UserName"] = Current.UserName = "101112TLN3";
+            Session["CpnyID"] = Current.CpnyID = "101112TLN3";
             Session["Language"] = Current.Language = "vi";
             Session["LangID"] = 1;
         }
