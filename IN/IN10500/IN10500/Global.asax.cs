90de942084d38490b5a4b115f8eb90ca4af81a6d
langhx:4 <langhx:4@hqsoft.tfs.local>
2016-11-03 04:43:42 +0000

    git-tfs-id: [https://hqsofttfs.hqsoft.com.vn/tfs/HQSOFT]$/Cloud/eSky4D;C43266

--- IN/IN10500/IN10500/Global.asax.cs
+++ IN/IN10500/IN10500/Global.asax.cs
@@ -39,14 +39,14 @@ namespace IN10500
 
             Current.Authorize = false;
             Current.Server = "EARTHSVR\\SQL2012";// ConfigurationManager.AppSettings["Server"].ToString();
-            Current.DBSys = "eBiz4DCloudSysJFV";//ConfigurationManager.AppSettings["DBSys"].ToString(); ND_eSky4DSys eBiz4DCloudSysJFV
+            Current.DBSys = "ND_eSky4DSys";//ConfigurationManager.AppSettings["DBSys"].ToString(); ND_eSky4DSys eBiz4DCloudSysJFV
             Current.FormatDate = "MM-dd-yyyy";
             AccessRight acc = new AccessRight() ;
             acc.Delete = true;
             acc.Insert = true;
             acc.Update = true;
             Session["IN10500"] = acc;
-            Session["DBApp"] = Current.DBApp = "eBiz4DCloudAppJFV";// "eBiz4DWebApp";Kowil_eSky4DApp ND_eSky4DApp eBiz4DCloudAppJFV
+            Session["DBApp"] = Current.DBApp = "ND_eSky4DApp";// "eBiz4DWebApp";Kowil_eSky4DApp ND_eSky4DApp eBiz4DCloudAppJFV
             Session["UserName"] = Current.UserName = "admin";
             Session["CpnyID"] = Current.CpnyID = "10106TMT1";
             Session["Language"] = Current.Language = "vi";
