4437181c29aa6c39263d887b2c771df39a085d6d
phuchd <phuchd@hqsoft.tfs.local>
2016-11-14 02:47:43 +0000

    Fix bugs + chuan code
    git-tfs-id: [https://hqsofttfs.hqsoft.com.vn/tfs/HQSOFT]$/Cloud/eSky4D;C43472

--- SA/SA03200/SA03200/Scripts/Screen/SA03200.js
+++ SA/SA03200/SA03200/Scripts/Screen/SA03200.js
@@ -2,10 +2,31 @@
 var keys = ['PDAID', 'BranchID', 'SlsperId'];
 var fieldsCheckRequire = ["PDAID", "BranchID", "SlsperId", "LicenseKey"];
 var fieldsLangCheckRequire = ["PDAID", "BranchID", "SlsperId", "LicenseKey"];
+
+var _Source = 0;
+var _maxSource = 1;
+var _isLoadMaster = false;
 ///////////////////////////////////////////////////////////////////////
 //// Store /////////////////////////////////////////////////////////////
+
 ////////////////////////////////////////////////////////////////////////
 //// Event /////////////////////////////////////////////////////////////
+var checkLoad = function () {
+    _Source += 1;
+    if (_Source == _maxSource) {
+        _isLoadMaster = true;
+        _Source = 0;
+        App.stoPPC_License.reload();
+        HQ.common.showBusy(false);
+    }
+};
+
+var firstLoad = function () {
+    HQ.util.checkAccessRight();
+    HQ.isFirstLoad = true;
+    HQ.common.showBusy(true, HQ.common.getLang("loadingData"));
+    App.cboBranchID.getStore().addListener('load', checkLoad);
+};
 
 var menuClick = function (command) {
     switch (command) {
@@ -26,24 +47,23 @@ var menuClick = function (command) {
                 HQ.message.show(20150303, '', 'refresh');
             }
             else {
-                HQ.isChange = false;
-                HQ.isFirstLoad = true;
-                App.stoPPC_License.reload();
+                refresh('yes');
             }
             break;
         case "new":
             if (HQ.isInsert) {
                 HQ.grid.insert(App.grdPPC_License, keys);
-                //App.txtLastSyncDate.value(_dateServer);
-                //App.txtLastSyncDate.set('LastSyncDate', _dateServer);
-                //item.set("LastSyncDate", new Date(_dateServer));
             }
             break;
         case "delete":
-            if (App.slmPPC_License.selected.items[0] != undefined) {
-                var rowindex = HQ.grid.indexSelect(App.grdPPC_License);
-                if (rowindex != '')
-                    HQ.message.show(2015020807, [HQ.grid.indexSelect(App.grdPPC_License), ''], 'deleteData', true)
+            if (HQ.isDelete) {
+                if (App.slmPPC_License.selected.items[0] != undefined) {
+                    if (App.slmPPC_License.selected.items[0].data.PDAID != "" &&
+                        App.slmPPC_License.selected.items[0].data.BranchID != "" &&
+                        App.slmPPC_License.selected.items[0].data.SlsperId != "") {
+                        HQ.message.show(2015020806, [HQ.grid.indexSelect(App.grdPPC_License)], 'deleteData', true);
+                    }
+                }
             }
             break;
         case "save":
@@ -75,6 +95,31 @@ var StatusCheckAll_Change = function (sender, value) {
     }
 };
 
+var stoChanged = function (sto) {
+    HQ.isChange = HQ.store.isChange(sto);
+    HQ.common.changeData(HQ.isChange, 'SA03200');
+};
+
+var stoLoad = function (sto) {
+    App.StatusCheckAll.setValue(false);
+    HQ.isChange = HQ.store.isChange(sto);
+    HQ.common.changeData(HQ.isChange, 'SA03200');
+    if (HQ.isFirstLoad) {
+        if (HQ.isInsert) {
+            //HQ.store.insertBlank(sto, keys);
+            HQ.store.insertRecord(sto, keys, { LastSyncDate: new Date(HQ.bussinessDate) });
+        }
+        HQ.isFirstLoad = false;
+    }
+    if (_isLoadMaster) {
+        HQ.common.showBusy(false);
+    }
+};
+
+var stoBeforeLoad = function (sto) {
+    HQ.common.showBusy(true, HQ.common.getLang('loadingData'));
+};
+
 var grdPPC_License_BeforeEdit = function (editor, e) {
     if (e.field == 'SlsperId') {
         if (e.record.data.BranchID) {
@@ -86,16 +131,20 @@ var grdPPC_License_BeforeEdit = function (editor, e) {
     }
     return HQ.grid.checkBeforeEdit(e, keys);
 };
+
 var grdPPC_License_Edit = function (item, e) {
     HQ.grid.checkInsertKey(App.grdPPC_License, e, keys);
 };
+
 var grdPPC_License_ValidateEdit = function (item, e) {
     return HQ.grid.checkValidateEdit(App.grdPPC_License, e, keys, false);
 };
+
 var grdPPC_License_Reject = function (record) {
     HQ.grid.checkReject(record, App.grdPPC_License);
     stoChanged(App.stoPPC_License);
 };
+
 /////////////////////////////////////////////////////////////////////////
 //// Process Data ///////////////////////////////////////////////////////
 var save = function () {
@@ -108,8 +157,7 @@ var save = function () {
             },
             success: function (msg, data) {
                 HQ.message.show(201405071);
-                HQ.isChange = false;
-                menuClick("refresh");
+                refresh('yes');
             },
             failure: function (msg, data) {
                 HQ.message.process(msg, data, true);
@@ -125,34 +173,6 @@ var deleteData = function (item) {
     }
 };
 
-//load khi giao dien da load xong, gan  HQ.isFirstLoad=true de biet la load lan dau
-var firstLoad = function () {
-    HQ.isFirstLoad = true;
-    App.stoPPC_License.reload();
-}
-//khi có sự thay đổi thêm xóa sửa trên lưới gọi tới để set * cho header de biết đã có sự thay đổi của grid
-var stoChanged = function (sto) {
-    HQ.isChange = HQ.store.isChange(sto);
-    HQ.common.changeData(HQ.isChange, 'SA03200');
-};
-//load lai trang, kiem tra neu la load lan dau thi them dong moi vao
-var stoLoad = function (sto) {
-    HQ.common.showBusy(false);
-    App.StatusCheckAll.setValue(false);
-    HQ.isChange = HQ.store.isChange(sto);
-    HQ.common.changeData(HQ.isChange, 'SA03200');
-    if (HQ.isFirstLoad) {
-        if (HQ.isInsert) {
-            //HQ.store.insertBlank(sto, keys);
-            HQ.store.insertRecord(sto, keys, { LastSyncDate: new Date(HQ.bussinessDate) });
-        }
-        HQ.isFirstLoad = false;
-    }
-};
-//trước khi load trang busy la dang load data
-var stoBeforeLoad = function (sto) {
-    HQ.common.showBusy(true, HQ.common.getLang('loadingdata'));
-};
 
 /////////////////////////////////////////////////////////////////////////
 //// Other Functions ////////////////////////////////////////////////////
