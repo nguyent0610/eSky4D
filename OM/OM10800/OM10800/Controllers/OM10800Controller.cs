304803ccb8b850e62480f2634236d6885f24633d
7ac0d68e-e206-4451-9727-1c1e8708da9e <7ac0d68e-e206-4451-9727-1c1e8708da9e>
2016-12-27 10:23:11 +0000

    git-tfs-id: [https://hqsofttfs.hqsoft.com.vn/tfs/HQSOFT]$/Cloud/eSky4D;C45612

---
+++ OM/OM10800/OM10800/Controllers/OM10800Controller.cs
@@ -0,0 +1,282 @@
+using HQ.eSkyFramework;
+using Ext.Net;
+using Ext.Net.MVC;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PartialViewResult = System.Web.Mvc.PartialViewResult;
+using System.IO;
+using System.Text;
+using Aspose.Cells;
+using HQFramework.DAL;
+using HQFramework.Common;
+using System.Drawing;
+using System.Globalization;
+
+namespace OM10800.Controllers
+{
+    [DirectController]
+    [CustomAuthorize]
+    [CheckSessionOut]
+    public class OM10800Controller : Controller
+    {
+        private string _screenNbr = "OM10800";
+        private string _userName = Current.UserName;
+        OM10800Entities _db = Util.CreateObjectContext<OM10800Entities>(false);
+        private JsonResult _logMessage;
+        public ActionResult Index()
+        {
+          
+            Util.InitRight(_screenNbr);
+            return View();
+        }
+
+        //[OutputCache(Duration = 1000000, VaryByParam = "lang")]
+        public PartialViewResult Body(string lang)
+        {
+            return PartialView();
+        }
+        public ActionResult GetHeader(string branchID, string batNbr)
+        {
+            var obj = _db.OM10800_pdHeader(Current.CpnyID, Current.UserName, Current.LangID, branchID,batNbr).FirstOrDefault();
+            return this.Store(obj);
+        }
+        public ActionResult GetOrder(string branchID, string batNbr, DateTime startDate, DateTime endDate)
+        {
+            _db.CommandTimeout = int.MaxValue;
+            var orders = _db.OM10800_pgOrder(Current.UserName, Current.CpnyID, Current.LangID,branchID,batNbr, startDate, endDate).ToList();
+            return this.Store(orders);
+        }
+        public ActionResult GetDet(string branchID, string batNbr,
+            DateTime startDate, DateTime endDate)
+        {
+            _db.CommandTimeout = int.MaxValue;
+            var dets = _db.OM10800_pgDet(Current.UserName, Current.CpnyID, Current.LangID, branchID, batNbr,startDate, endDate).ToList();
+            return this.Store(dets);
+        }
+        public ActionResult GetDelivery(string branchID, string batNbr)
+        {
+            _db.CommandTimeout = int.MaxValue;
+            var dets = _db.OM10800_pgDelivery(Current.UserName, Current.CpnyID, Current.LangID, branchID, batNbr).ToList();
+            return this.Store(dets);
+        }
+        [HttpPost]
+        public ActionResult DeleteHeader(FormCollection data)
+        {
+            try
+            {
+                string BranchID = data["cboCpnyID"].PassNull();
+                string BatNbr = data["cboBatNbr"].PassNull();
+
+                var batch = _db.Batches.FirstOrDefault(p => p.BranchID == BranchID && p.BatNbr == BatNbr && p.Module == "OM");
+                if (batch != null)
+                {
+                    _db.Batches.DeleteObject(batch);
+                }
+
+                var lstOM_ShipLine = _db.OM_ShipLine.Where(p => p.BranchID == BranchID && p.BatNbr == BatNbr).ToList();
+                foreach (var item in lstOM_ShipLine)
+                {
+                    _db.OM_ShipLine.DeleteObject(item);
+                }
+
+                var lstOM_ShipDelivery = _db.OM_ShipDelivery.Where(p => p.BranchID == BranchID && p.BatNbr == BatNbr).ToList();
+                foreach (var item in lstOM_ShipDelivery)
+                {
+                    _db.OM_ShipDelivery.DeleteObject(item);
+                }
+
+                _db.SaveChanges();
+                return Util.CreateMessage(MessageProcess.Delete, BatNbr);
+            }
+            catch (Exception ex)
+            {
+                if (ex is MessageException) return (ex as MessageException).ToMessage();
+                return Json(new { success = false, type = "error", errorMsg = ex.ToString() });
+            }
+        }
+        [HttpPost]
+        public ActionResult Save(FormCollection data)
+        {
+            try
+            {
+                StoreDataHandler dataHandler = new StoreDataHandler(data["lstHeader"]);
+                var curHeader = dataHandler.ObjectData<OM10800_pdHeader_Result>().FirstOrDefault();
+                var CpnyID = data["cboCpnyID"].PassNull().ToUpper().Trim();
+                var Handle = data["cboHandle"].PassNull().ToUpper().Trim();
+
+                StoreDataHandler dataHandler1 = new StoreDataHandler(data["lstOrder"]);
+                ChangeRecords<OM10800_pgOrder_Result> lstOrder = dataHandler1.BatchObjectData<OM10800_pgOrder_Result>();
+
+                StoreDataHandler dataHandler2 = new StoreDataHandler(data["lstDelivery"]);
+                ChangeRecords<OM10800_pgDelivery_Result> lstDelivery = dataHandler2.BatchObjectData<OM10800_pgDelivery_Result>();
+
+                #region Save Header Company
+                var header = _db.Batches.FirstOrDefault(p => p.BranchID == CpnyID && p.BatNbr==curHeader.BatNbr && p.Module=="OM");
+                if (header != null)
+                {
+                    if (header.tstamp.ToHex() == curHeader.tstamp.ToHex())
+                    {
+                        Updating_Batch(ref header, curHeader);
+                    }
+                    else
+                    {
+                        throw new MessageException(MessageType.Message, "19");
+                    }
+                }
+                else
+                {
+                    header = new Batch();
+                    header.ResetET();
+                    header.BranchID = CpnyID;
+                    header.BatNbr = _db.OMNumbering(CpnyID, "Shipper", "SH").FirstOrDefault();
+                    header.Module = "OM";
+
+                    header.Crtd_DateTime = DateTime.Now;
+                    header.Crtd_Prog = _screenNbr;
+                    header.Crtd_User = _userName;
+                    Updating_Batch(ref header, curHeader);
+                    _db.Batches.AddObject(header);
+                }
+                #endregion
+
+                #region Save Order
+
+                lstOrder.Created.AddRange(lstOrder.Updated);
+
+                foreach (OM10800_pgOrder_Result curRow in lstOrder.Created)
+                {
+
+                    var RowDB = _db.OM_ShipLine.FirstOrDefault(p => p.BranchID == header.BranchID && p.BatNbr == header.BatNbr && p.OrdNbr == curRow.OrderNbr);
+                    if (curRow.Selected.Value)
+                    {
+                        if (RowDB != null)
+                        {
+                            if (RowDB.tstamp.ToHex() == curRow.tstamp.ToHex())
+                            {
+                                Updating_OM_ApproveOrder(RowDB, curRow);
+                            }
+                            else
+                            {
+                                throw new MessageException(MessageType.Message, "19");
+                            }
+                        }
+                        else
+                        {
+                            RowDB = new OM_ShipLine();
+                            RowDB.ResetET();
+                            RowDB.BatNbr = header.BatNbr;
+                            RowDB.BranchID = header.BranchID;
+                            RowDB.OrdNbr = curRow.OrderNbr;
+                            RowDB.Crtd_DateTime = DateTime.Now;
+                            RowDB.Crtd_Prog = _screenNbr;
+                            RowDB.Crtd_User = _userName;
+
+                            Updating_OM_ApproveOrder(RowDB, curRow);
+                            _db.OM_ShipLine.AddObject(RowDB);
+                        }
+                    }
+                    else
+                    {
+                        if (RowDB != null)
+                            _db.OM_ShipLine.DeleteObject(RowDB);
+                    }
+                }
+                #endregion
+
+                #region Save Delivery
+
+
+                lstDelivery.Created.AddRange(lstDelivery.Updated);
+
+                foreach (OM10800_pgDelivery_Result curRow in lstDelivery.Created)
+                {
+
+                    var RowDB = _db.OM_ShipDelivery.FirstOrDefault(p => p.BranchID == header.BranchID && p.BatNbr == header.BatNbr && p.SlsperID == curRow.SlsPerID);
+                    if (curRow.Selected.Value)
+                    {
+                        if (RowDB != null)
+                        {
+                            if (RowDB.tstamp.ToHex() == curRow.tstamp.ToHex())
+                            {
+                                Updating_OM_ShipDelivery(RowDB, curRow);
+                            }
+                            else
+                            {
+                                throw new MessageException(MessageType.Message, "19");
+                            }
+                        }
+                        else
+                        {
+                            RowDB = new OM_ShipDelivery();
+                            RowDB.ResetET();
+                            RowDB.BatNbr = header.BatNbr;
+                            RowDB.BranchID = header.BranchID;
+                            RowDB.SlsperID = curRow.SlsPerID;
+                            RowDB.Crtd_DateTime = DateTime.Now;
+                            RowDB.Crtd_Prog = _screenNbr;
+                            RowDB.Crtd_User = _userName;
+
+                            Updating_OM_ShipDelivery(RowDB, curRow);
+                            _db.OM_ShipDelivery.AddObject(RowDB);
+                        }
+                    }
+                    else
+                    {
+                        if (RowDB != null)
+                            _db.OM_ShipDelivery.DeleteObject(RowDB);
+                    }
+                }
+                #endregion
+                if (Handle.PassNull() != "" && Handle.PassNull() != "N")
+                    header.Status = Handle;
+                _db.SaveChanges();
+                return Util.CreateMessage(MessageProcess.Save, header);
+
+            }
+            catch (Exception ex)
+            {
+                if (ex is MessageException) return (ex as MessageException).ToMessage();
+                return Json(new { success = false, type = "error", errorMsg = ex.ToString() });
+            }
+        }
+
+
+        private void Updating_Batch(ref Batch t, OM10800_pdHeader_Result s)
+        {          
+            t.Descr = s.Descr;         
+            t.DateEnt = s.DateEnt;
+            t.EditScrnNbr = _screenNbr;
+            t.JrnlType = "OM";
+            t.Rlsed = 0;
+            t.Status =s.Status;
+            t.RefNbr = s.RefNbr;//so xe
+
+            t.LUpd_DateTime = DateTime.Now;
+            t.LUpd_Prog = _screenNbr;
+            t.LUpd_User = _userName;
+
+        }
+        private void Updating_OM_ApproveOrder(OM_ShipLine t, OM10800_pgOrder_Result s)
+        {                
+            t.LUpd_DateTime = DateTime.Now;
+            t.LUpd_Prog = _screenNbr;
+            t.LUpd_User = _userName;
+        }
+
+        private void Updating_OM_ShipDelivery(OM_ShipDelivery t, OM10800_pgDelivery_Result s)
+        {
+
+          
+
+            t.LUpd_DateTime = DateTime.Now;
+            t.LUpd_Prog = _screenNbr;
+            t.LUpd_User = _userName;
+
+        }
+
+    }
+}
---
+++ OM/OM10800/OM10800/Scripts/Screen/OM10800.js
@@ -0,0 +1,349 @@
+﻿//// Declare //////////////////////////////////////////////////////////
+var keys = ['RoleID'];
+var fieldsCheckRequire = ["RoleID","Desc"];
+var fieldsLangCheckRequire = ["RoleID","Desc"];
+
+var _Source = 0;
+var _maxSource = 3;
+var _SourceByCboCpnyID = 0;
+var _maxSourceByCboCpnyID = 2;
+var _maxSource = 3;
+var _SourceData = 0;
+var _maxSourceData = 4;
+
+
+///////////////////////////////////////////////////////////////////////
+//// Store /////////////////////////////////////////////////////////////
+var checkLoad = function (sto) {
+    _Source += 1;
+    if (_Source == _maxSource) {
+        _Source = 0;      
+        HQ.common.showBusy(false);
+        App.cboCpnyID.setValue(HQ.cpnyID);
+    }
+};
+var checkLoadBycboCpnyID = function (sto) {
+    _SourceByCboCpnyID++;
+    if (_SourceByCboCpnyID == _maxSourceByCboCpnyID) {
+        App.cboBatNbr.setValue(HQ.batNbr);
+        HQ.common.showBusy(false);
+    }
+}
+
+var stoData_Load = function (sto) {
+    
+    _SourceData++;
+    if (_SourceData == _maxSourceData) {
+        setTimeout(function () {
+            App.slmOrder.select(-1);
+            App.slmOrder.select(0);
+        }, 300);
+     
+        if (App.stoHeader.data.items.length > 0) {
+           
+            bindHeader(false);
+        }
+        else bindHeader(true);
+    }
+};
+var checkLoadDatacboCpnyID_Change = function () {//load cac master phụ thuộc vào combo là key của màn hình rồi mới bind dữ liệu lên    
+    HQ.common.showBusy(true, HQ.common.getLang("loadingData"));
+    _SourceByCboCpnyID = 0;//dem lai source data load
+    App.cboBatNbr.getStore().reload();
+    App.cboLicensePlate.getStore().reload();
+    
+};
+
+
+var checkLoadData_Change = function () {//load cac master phụ thuộc vào combo là key của màn hình rồi mới bind dữ liệu lên
+    if (App.cboBatNbr.rawValue == "")//truong hop new moi
+    {
+        loadData();
+    } else {
+        loadData();
+    }
+
+};
+var loadData = function () {
+    HQ.common.showBusy(true, HQ.common.getLang("loadingData"));
+    _SourceData = 0;//dem lai source data load
+    App.stoHeader.reload();
+    App.stoOrder.reload();
+    App.stoDet.reload();
+    App.stoDelivery.reload();
+   
+}
+////////////////////////////////////////////////////////////////////////
+//// Event /////////////////////////////////////////////////////////////
+
+var menuClick = function (command) {
+    switch (command) {
+        case "first":
+                HQ.grid.first(App.grdSYS_Role);
+            break;
+        case "prev":
+                HQ.grid.prev(App.grdSYS_Role);
+            break;
+        case "next":
+                HQ.grid.next(App.grdSYS_Role);
+            break;
+        case "last":
+                HQ.grid.last(App.grdSYS_Role);
+            break;
+        case "refresh":
+            if (HQ.isChange) {
+                HQ.message.show(20150303, '', 'refresh');
+            }
+            else {
+                refresh('yes');
+            }          
+            break;
+        case "new":
+            if (HQ.isInsert) {
+                App.cboBatNbr.setValue('');
+            }
+            break;
+        case "delete":
+            if (HQ.isDelete) {
+                if (App.cboStatus.getValue() == 'H' && App.cboBatNbr.getValue())
+                HQ.message.show(11, '', 'deleteData');
+            }
+            break;
+        case "save":
+            if (HQ.isUpdate || HQ.isInsert || HQ.isDelete) {
+                if (HQ.form.checkRequirePass(App.frmMain)) {
+                    save();
+                }
+            }
+            break;
+        case "print":
+            break;
+        case "close":
+            HQ.common.close(this);            
+            break;
+    }
+};
+
+//load khi giao dien da load xong, gan  HQ.isFirstLoad=true de biet la load lan dau
+var firstLoad = function () {
+    HQ.util.checkAccessRight();
+    App.frmMain.isValid();
+    HQ.common.showBusy(true, HQ.common.getLang("loadingData"));
+    App.cboCpnyID.getStore().addListener('load', checkLoad);
+    App.cboHandle.getStore().addListener('load', checkLoad);
+    App.cboStatus.getStore().addListener('load', checkLoad);
+
+    App.cboBatNbr.getStore().addListener('load', checkLoadBycboCpnyID);
+    App.cboLicensePlate.getStore().addListener('load', checkLoadBycboCpnyID);
+
+    App.cboCpnyID.getStore().reload();
+    App.cboHandle.getStore().reload();
+    App.cboStatus.getStore().reload();
+
+
+
+};
+
+var frmChange = function () {    
+    HQ.isChange = HQ.store.isChange(App.stoDet) || HQ.store.isChange(App.stoDelivery);
+    HQ.common.changeData(HQ.isChange, 'OM10800');//co thay doi du lieu gan * tren tab title header
+    if (App.cboStatus.getValue() == 'H') {
+        if (HQ.isChange) {
+            App.btnLoad.disable();
+        } else App.btnLoad.enable();
+    }
+};
+
+function cboCpnyID_Change(items, newValue, oldValue) {
+    if ((items.rawValue == "" || !items.hasFocus)) {// ko phai la truong hop new, neu xoa combo thi new moi hoặc trường hợp ko focus combo co thay doi thi load lai source        
+        checkLoadDatacboCpnyID_Change();
+    }
+}
+function cboCpnyID_Select(items, newValue, oldValue) {
+    checkLoadDatacboCpnyID_Change();
+}
+//khi nhan combo xo ra, neu da thay doi thi ko xo ra
+function cboCpnyID_Expand(sender, value) {
+    if (HQ.isChange) {
+        App.cboCpnyID.collapse();
+    }
+};
+
+function cboBatNbr_Change(items, newValue, oldValue) {
+    if ((items.rawValue == "" || !items.hasFocus)) {// ko phai la truong hop new, neu xoa combo thi new moi hoặc trường hợp ko focus combo co thay doi thi load lai source        
+        checkLoadData_Change();
+    }
+}
+function cboBatNbr_Select(items, newValue, oldValue) {
+    checkLoadData_Change();
+}
+//khi nhan combo xo ra, neu da thay doi thi ko xo ra
+function cboBatNbr_Expand(sender, value) {
+    if (HQ.isChange) {
+        App.cboBatNbr.collapse();
+    }
+};
+
+var slmOrder_Select= function (slm, selRec, idx, eOpts) {   
+    App.grdDet.store.filterBy(function (record) {
+        if (record.data.OrderNbr == selRec.data.OrderNbr) {
+            return record;
+        }
+    });
+}
+var chkSelectHeaderOrder_Change = function (chk, newValue, oldValue, eOpts) {
+    if (App.cboStatus.getValue() != 'H') return;
+    App.stoOrder.suspendEvents();
+    var allData = App.stoOrder.snapshot || App.stoOrder.allData || App.stoOrder.data;
+    allData.each(function (record) {
+        record.data.Selected = chk.value;
+    });
+    App.stoOrder.resumeEvents();
+    App.grdOrder.view.refresh();
+}
+var chkSelectHeaderDelivery_Change = function (chk, newValue, oldValue, eOpts) {
+    if (App.cboStatus.getValue() != 'H') return;
+    App.stoDelivery.suspendEvents();
+    var allData = App.stoDelivery.snapshot || App.stoDelivery.allData || App.stoDelivery.data;
+    allData.each(function (record) {
+        record.data.Selected = chk.value;
+    });
+    App.stoDelivery.resumeEvents();
+    App.grdDelivery.view.refresh();
+}
+var grdOrder_BeforeEdit = function (editor, context, eOpts) {
+    if (App.cboStatus.getValue() != 'H') return false;
+}
+var grdDelivery_BeforeEdit = function (editor, context, eOpts) {
+    if (App.cboStatus.getValue() != 'H') return false;
+}
+
+var btnLoad_click = function () {
+    HQ.common.showBusy(true, HQ.common.getLang("loadingData"));
+   
+    _SourceData = 1;
+    App.stoOrder.reload();
+    App.stoDet.reload();
+    App.stoDelivery.reload();
+}
+//// Process Data ///////////////////////////////////////////////////////
+// Xử lí các hàm liên quan tới Controller hoặc các nút trên menu
+var bindHeader = function (isNew) {
+    App.frmMain.suspendEvents();
+    HQ.isNew = false;
+    HQ.common.lockItem(App.frmMain, false);
+    App.cboBatNbr.forceSelection = true;
+    //các combo phu thuoc de phai clear het filter de lay gia tri cho dung
+    App.cboHandle.getStore().clearFilter();  
+    if (isNew) {
+        App.stoHeader.clearData();
+        HQ.store.insertBlank(App.stoHeader);
+        record = App.stoHeader.getAt(0);
+        record.data.Status = 'H';
+        record.data.DateEnt = HQ.bussinessDate;
+        HQ.isNew = true; //record la new 
+        HQ.isFirstLoad = true;
+        App.cboBatNbr.forceSelection = false;
+        HQ.common.setRequire(App.frmMain);  //to do cac o la require            
+        if (!App.cboBatNbr.hasFocus) App.cboBatNbr.focus(true); //focus ma khi tao moi
+        App.stoHeader.commitChanges();
+    }
+    else if(!App.cboBatNbr.getValue()) App.frmMain.getForm().updateRecord();
+    var record = App.stoHeader.getAt(0);
+    //filter cho cac combo phu thuoc    
+    App.cboHandle.store.filter('Status', record.data.Status);
+    App.cboHandle.setValue('N');
+    App.frmMain.getForm().loadRecord(record);
+
+    if (!HQ.isInsert && HQ.isNew) {
+        App.cboBatNbr.forceSelection = true;
+        HQ.common.lockItem(App.frmMain, true);
+    }
+    else if (!HQ.isUpdate && !HQ.isNew) {
+        HQ.common.lockItem(App.frmMain, true);
+    }
+    
+    if (record.data.Status != 'H') {
+        App.cboBatNbr.forceSelection = true;
+        HQ.common.lockItem(App.frmMain, true);
+    }
+
+    HQ.common.showBusy(false);
+    frmChange();
+    
+    App.frmMain.resumeEvents();
+
+}
+var save = function () {
+    var objOrder = HQ.store.findRecord(App.stoOrder, ['Selected'], [true]);
+    if (!objOrder) {
+        HQ.message.show(1000, App.tabInfo.items.items[0].title, '');
+        return;
+    }
+    var objDelivery = HQ.store.findRecord(App.stoDelivery, ['Selected'], [true]);
+    if (!objDelivery) {
+        HQ.message.show(1000, App.tabInfo.items.items[1].title, '');
+        return;
+    }
+    App.frmMain.getForm().updateRecord();
+    if (App.frmMain.isValid()) {
+        App.frmMain.submit({
+            waitMsg: HQ.common.getLang("WaitMsg"),
+            url: 'OM10800/Save',
+            params: {
+                lstHeader: Ext.encode(App.stoHeader.getRecordsValues()),
+                lstOrder: HQ.store.getData(App.stoOrder),
+                lstDelivery: HQ.store.getData(App.stoDelivery)
+            },
+            success: function (msg, data) {
+                HQ.message.process(msg, data, true);
+                if (!App.cboBatNbr.getValue()) {
+                    _SourceByCboCpnyID = 1;
+                    HQ.batNbr = data.result.data.BatNbr;
+                    App.cboBatNbr.store.reload();//load lai combo de co du lieu;
+                } else {
+                    refresh('yes')
+                }
+            },
+            failure: function (msg, data) {
+                HQ.message.process(msg, data, true);
+            }
+        });
+    }
+};
+
+var deleteData = function (item) {
+    if (item == "yes") {
+        App.frmMain.submit({
+            waitMsg: HQ.common.getLang("DeletingData"),
+            url: 'OM10800/DeleteHeader',
+            timeout: 7200,
+            success: function (msg, data) {
+                _SourceByCboCpnyID = 1;
+                HQ.batNbr = '';
+                App.cboBatNbr.store.reload();//load lai combo de co du lieu;               
+            },
+            failure: function (msg, data) {
+                HQ.message.process(msg, data, true);
+            }
+        });
+    }
+};
+
+function refresh(item) {
+    if (item == 'yes') {
+        HQ.common.showBusy(true, HQ.common.getLang('loadingData'));
+        loadData();
+    }
+};
+/////////////////////////////////////////////////////////////////////////
+//// Other Functions ////////////////////////////////////////////////////
+//Các hàm tính toán, hỗ trợ cho các sự kiện của chương trình
+var renderRowNumber = function (value, meta, record) {
+    return App.stoDet.data.indexOf(record) + 1;
+}
+var stringFilter = function (record) {
+    return HQ.grid.filterString(record, this);
+}
+/////////////////////////////////////////////////////////////////////////
+
