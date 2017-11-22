880a1a6188417755ad9e444e17f01658ae4caaaf
5e40a53d-b7f1-4d3b-8427-d5f1ea2187d9 <5e40a53d-b7f1-4d3b-8427-d5f1ea2187d9>
2017-11-22 07:39:36 +0000

    Đổi mã ngôn ngữ
    git-tfs-id: [https://hqsofttfs.hqsoft.com.vn/tfs/HQSOFT]$/Cloud/eSky4D;C54046

--- IN/IN20300/IN20300/Global.asax.cs
+++ IN/IN20300/IN20300/Global.asax.cs
@@ -38,7 +38,7 @@ namespace IN20300
             //Session["LangID"] = 1;
 
             Current.Authorize = false;
-            Current.Server = "HOANGSAD";// ConfigurationManager.AppSettings["Server"].ToString();
+            Current.Server = "TRUONGSAD";// ConfigurationManager.AppSettings["Server"].ToString();
             Current.DBSys = "ND_Test_eSky4DSys";//ConfigurationManager.AppSettings["DBSys"].ToString();
             AccessRight acc = new AccessRight();
             acc.Delete = true;
