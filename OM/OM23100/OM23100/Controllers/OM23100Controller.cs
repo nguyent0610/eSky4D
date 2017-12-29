53ac908011b6342eb5f2ea54344d78fdafe3281c
5e40a53d-b7f1-4d3b-8427-d5f1ea2187d9 <5e40a53d-b7f1-4d3b-8427-d5f1ea2187d9>
2017-12-29 09:31:05 +0000

    update dll
    git-tfs-id: [https://hqsofttfs.hqsoft.com.vn/tfs/HQSOFT]$/Cloud/eSky4D;C54650

--- OM/OM23100/OM23100/Controllers/OM23100Controller.cs
+++ OM/OM23100/OM23100/Controllers/OM23100Controller.cs
@@ -30,7 +30,7 @@ namespace OM23100.Controllers
         private JsonResult _logMessage;
         public ActionResult Index()
         {
-           
+            LicenseHelper.ModifyInMemory.ActivateMemoryPatching();
             Util.InitRight(_screenNbr);
             return View();
         }
