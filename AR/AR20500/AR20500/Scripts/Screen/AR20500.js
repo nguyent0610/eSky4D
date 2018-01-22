3d3e835fe7aa046d67b7d21753a7a6ac371d5af1
31d17e47-3b86-431c-8d44-6e8256b8c09d <31d17e47-3b86-431c-8d44-6e8256b8c09d>
2018-01-22 03:27:53 +0000

    sửa lổi không load dc hình khi lat và lng =0, lỗi mất data ngành
    git-tfs-id: [https://hqsofttfs.hqsoft.com.vn/tfs/HQSOFT]$/Cloud/eSky4D;C54974

--- AR/AR20500/AR20500/Scripts/Screen/AR20500.js
+++ AR/AR20500/AR20500/Scripts/Screen/AR20500.js
@@ -947,9 +947,11 @@ var slmCust_Select = function (rowModel, record, index, eOpts) {
     if (record[0]) {
         if (record[0].data.Lat && record[0].data.Lng) {
             Gmap.Process.navMapCenterByLocation(record[0].data.Lat, record[0].data.Lng, record.index + 1);
-            displayImage(App.imgImages, record[0].data.ImageFileName);// get image theo binary
-            displayImage(App.imgImages1, record[0].data.BusinessPic);// get image theo binary
+            //displayImage(App.imgImages, record[0].data.ImageFileName);// get image theo binary
+            //displayImage(App.imgImages1, record[0].data.BusinessPic);// get image theo binary
         }
+        displayImage(App.imgImages, record[0].data.ImageFileName);// get image theo binary
+        displayImage(App.imgImages1, record[0].data.BusinessPic);// get image theo binary
     }
 };
 
@@ -1555,3 +1557,6 @@ var cboBrandID_Focus = function () {
         this.forceSelection = true;
     }
 }
+
+
+
