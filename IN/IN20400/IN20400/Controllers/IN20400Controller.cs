cc5c90def5f24a4f10c7a318d73061ce25fb75dd
a181d854-1df9-460e-99a3-23926519ac10 <a181d854-1df9-460e-99a3-23926519ac10>
2018-06-05 04:13:34 +0000

    IN20400 Vị trí kho Phú Thái
    git-tfs-id: [https://hqsofttfs.hqsoft.com.vn/tfs/HQSOFT]$/Cloud/eSky4D;C56395

---
+++ IN/IN20400/IN20400/Controllers/IN20400Controller.cs
@@ -0,0 +1,158 @@
+using HQ.eSkyFramework;
+using Ext.Net;
+using Ext.Net.MVC;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PartialViewResult = System.Web.Mvc.PartialViewResult;
+using System.IO;
+using System.Text;
+using System.Reflection;
+using System.Collections;
+using System.Runtime.Caching;
+namespace IN20400.Controllers
+{
+    [DirectController]
+    [CustomAuthorize]
+    [CheckSessionOut]
+    public class IN20400Controller : Controller
+    {
+        private string _screenNbr = "IN20400";
+        private string _userName = Current.UserName;
+        IN20400Entities _db = Util.CreateObjectContext<IN20400Entities>(false);
+        public ActionResult Index()
+        {
+            Util.InitRight(_screenNbr);
+            return View();
+        }
+        //[OutputCache(Duration = 1000000, VaryByParam = "lang")]
+        public PartialViewResult Body(string lang)
+        {
+            
+            return PartialView();
+        }
+        public ActionResult GetIN_SiteLocation(string siteID)
+        {
+            return this.Store(_db.IN20400_pgLoadSiteLocation(Current.CpnyID, Current.UserName, Current.LangID, siteID).ToList());
+        }
+        [HttpPost]
+        public ActionResult Save(FormCollection data)
+        {
+
+            try
+            {
+                string siteID = data["cboSiteID"].PassNull();
+
+                StoreDataHandler dataHandler = new StoreDataHandler(data["lstIN_SiteLocation"]);
+                ChangeRecords<IN20400_pgLoadSiteLocation_Result> lstLang = dataHandler.BatchObjectData<IN20400_pgLoadSiteLocation_Result>();
+                lstLang.Created.AddRange(lstLang.Updated);
+                foreach (IN20400_pgLoadSiteLocation_Result del in lstLang.Deleted)
+                {
+                    // neu danh sach them co chua danh sach xoa thi khong xoa thằng đó cập nhật lại tstamp của thằng đã xóa xem nhu trường hợp xóa thêm mới là trường hợp update
+                    if (lstLang.Created.Where(p => p.SiteID == siteID && p.WhseLoc == del.WhseLoc.ToUpper().Trim()).Count() > 0)
+                    {
+                        lstLang.Created.Where(p => p.SiteID == siteID && p.WhseLoc == del.WhseLoc.ToUpper().Trim()).FirstOrDefault().tstamp = del.tstamp;
+                    }
+                    else
+                    {
+                        var objDel = _db.IN_SiteLocation.ToList().Where(p => p.SiteID == siteID && p.WhseLoc == del.WhseLoc.ToUpper().Trim()).FirstOrDefault();
+                        if (objDel != null)
+                        {
+                            _db.IN_SiteLocation.DeleteObject(objDel);
+                        }
+                    }
+                }
+
+                foreach (IN20400_pgLoadSiteLocation_Result curLang in lstLang.Created)
+                {
+                    if (curLang.WhseLoc.PassNull() == "") continue;
+                    curLang.SiteID = siteID; 
+                    var lang = _db.IN_SiteLocation.Where(p => p.SiteID == siteID && p.WhseLoc == curLang.WhseLoc.ToUpper().Trim()).FirstOrDefault();
+
+                    if (lang != null)
+                    {
+                        if (lang.tstamp.ToHex() == curLang.tstamp.ToHex())
+                        {
+                            Update_Language(lang, curLang, false);
+                        }
+                        else
+                        {
+                            throw new MessageException(MessageType.Message, "19");
+                        }
+                    }
+                    else
+                    {
+                        lang = new IN_SiteLocation();
+                        Update_Language(lang, curLang, true);
+                        _db.IN_SiteLocation.AddObject(lang);
+                    }
+                }
+
+                _db.SaveChanges();
+
+                return Json(new { success = true });
+            }
+            catch (Exception ex)
+            {
+                if (ex is MessageException) return (ex as MessageException).ToMessage();
+                return Json(new { success = false, type = "error", errorMsg = ex.ToString() });
+            }
+        }
+        private void Update_Language(IN_SiteLocation t, IN20400_pgLoadSiteLocation_Result s, bool isNew)
+        {
+            if (isNew)
+            {
+                t.SiteID = s.SiteID;
+                t.WhseLoc = s.WhseLoc.ToUpper().Trim();
+                t.Crtd_DateTime = DateTime.Now;
+                t.Crtd_Prog = _screenNbr;
+                t.Crtd_User = _userName;
+            }
+            t.Descr = s.Descr;
+            t.SalesAllowed = true;
+            t.IssueAllowed = true;
+            t.LUpd_DateTime = DateTime.Now;
+            t.LUpd_Prog = _screenNbr;
+            t.LUpd_User = _userName;
+        }
+
+        [HttpPost]
+
+        public ActionResult DeleteAll(FormCollection data)
+        {
+
+            try
+            {
+
+                string SiteID = data["cboSiteID"].ToUpper().PassNull();
+
+
+                var lstSite = _db.IN_SiteLocation.Where(p => p.SiteID == SiteID).ToList();
+
+                foreach (var item in lstSite)
+                {
+
+                    _db.IN_SiteLocation.DeleteObject(item);
+
+                }
+                _db.SaveChanges();
+
+                return Util.CreateMessage(MessageProcess.Delete, SiteID);
+
+            }
+
+            catch (Exception ex)
+            {
+
+                if (ex is MessageException) return (ex as MessageException).ToMessage();
+
+                return Json(new { success = false, type = "error", errorMsg = ex.ToString() });
+
+            }
+
+        }
+    }
+}
---
+++ IN/IN20400/IN20400/Scripts/Screen/IN20400.js
@@ -0,0 +1,293 @@
+//// Declare //////////////////////////////////////////////////////////
+
+var keys = ['WhseLoc'];
+var fieldsCheckRequire = ["WhseLoc", "Descr"];
+var fieldsLangCheckRequire = ["WhseLoc", "Descr"];
+///////////////////////////////////////////////////////////////////////
+//// Store /////////////////////////////////////////////////////////////
+var _Source = 0;
+var _maxSource = 1;
+var _isLoadMaster = false;
+
+
+var checkLoad = function (sto) {
+    _Source += 1;
+    if (_Source == _maxSource) {
+        _isLoadMaster = true;
+        _Source = 0;
+        App.stoIN_SiteLocation.reload();
+        HQ.common.showBusy(false);
+    }
+};
+////////////////////////////////////////////////////////////////////////
+//// Event /////////////////////////////////////////////////////////////
+
+var menuClick = function (command) {
+    switch (command) {
+        case "first":
+            if (HQ.focus == 'header') {
+                HQ.isFirstLoad = true;
+                HQ.combo.first(App.cboSiteID, HQ.isChange);
+
+            }
+            else if (HQ.focus == 'grdIN_SiteLocation') {
+                HQ.grid.first(App.grdIN_SiteLocation);
+            }
+            break;
+        case "prev":
+            if (HQ.focus == 'header') {
+                HQ.isFirstLoad = true;
+                HQ.combo.prev(App.cboSiteID, HQ.isChange);
+
+            }
+            else if (HQ.focus == 'grdIN_SiteLocation') {
+                HQ.grid.prev(App.grdIN_SiteLocation);
+            }
+            break;
+        case "next":
+            if (HQ.focus == 'header') {
+                HQ.isFirstLoad = true;
+                HQ.combo.next(App.cboSiteID, HQ.isChange);
+
+            }
+            else if (HQ.focus == 'grdIN_SiteLocation') {
+                HQ.grid.next(App.grdIN_SiteLocation);
+            }
+            break;
+        case "last":
+            if (HQ.focus == 'header') {
+                HQ.isFirstLoad = true;
+                HQ.combo.last(App.cboSiteID, HQ.isChange);
+
+            }
+            else if (HQ.focus == 'grdIN_SiteLocation') {
+                HQ.grid.last(App.grdIN_SiteLocation);
+            }
+            break;
+        case "refresh":
+	
+            if (HQ.isChange) {
+                HQ.message.show(20150303, '', 'refresh');
+            }
+            else {
+                HQ.isChange = false;
+                HQ.isFirstLoad = true;
+                App.stoIN_SiteLocation.reload();
+            }          
+            break;
+        case "new":
+            if (HQ.isInsert && App.frmMain.isValid()) {
+                HQ.grid.insert(App.grdIN_SiteLocation, keys);
+            }
+            break;
+        case "delete":
+            if (HQ.focus == 'header') {
+                if (App.cboSiteID.getValue() != null) {
+
+                    HQ.message.show(11, '', 'deleteData');
+
+                } else {
+
+                    refresh('yes');
+
+                }
+            }
+            else if (HQ.focus == 'grdIN_SiteLocation')
+            {
+                if (App.slmIN_SiteLocation.selected.items[0] != undefined) {
+                    if (HQ.isDelete) {
+                        if (App.slmIN_SiteLocation.selected.items[0] != undefined) {
+                            if (App.slmIN_SiteLocation.selected.items[0].data.Code != "") {
+                                HQ.message.show(2015020806, [HQ.grid.indexSelect(App.grdIN_SiteLocation)], 'deleteData', true);
+                            }
+                        }
+                    }
+                }
+            }
+            break;
+        case "save":
+            if (HQ.isUpdate || HQ.isInsert || HQ.isDelete) {
+                if (HQ.store.checkRequirePass(App.stoIN_SiteLocation, keys, fieldsCheckRequire, fieldsLangCheckRequire)) {
+                    save();
+                }
+            }
+            break;
+        case "print":
+            break;
+        case "close":         
+            break;
+    }
+
+};
+var cboSiteID_TriggerClick = function (sender, value) {
+    if (HQ.isChange) {
+        HQ.message.show(150, '', '');
+    }
+    else {
+        App.cboSiteID.setValue('');
+        App.stoIN_SiteLocation.reload();
+    }
+};
+var cboSiteID_Change = function (sender, value) {
+    HQ.isFirstLoad = true;
+    App.stoIN_SiteLocation.reload();
+};
+//load khi giao dien da load xong, gan  HQ.isFirstLoad=true de biet la load lan dau
+var firstLoad = function () {
+    HQ.util.checkAccessRight(); // kiểm tra các quyền update,insert,del
+    HQ.isFirstLoad = true;
+    App.frmMain.isValid();
+    App.cboSiteID.getStore().addListener('load', checkLoad);
+    App.cboSiteID.getStore().reload();
+};
+
+var frmChange = function () {
+    HQ.isChange = HQ.store.isChange(App.stoIN_SiteLocation);
+    HQ.common.changeData(HQ.isChange, 'IN20400');//co thay doi du lieu gan * tren tab title header
+};
+function refresh(item) {
+    if (item == 'yes') {
+        HQ.isChange = false;
+        HQ.isFirstLoad = true;
+        App.stoIN_SiteLocation.reload();
+    }
+};
+
+
+//load lai trang, kiem tra neu la load lan dau thi them dong moi vao
+var stoLoad = function (sto) {
+    HQ.common.showBusy(false, HQ.common.getLang('loadingData'));
+    if (HQ.isFirstLoad) {
+        if (HQ.isInsert) {
+            HQ.store.insertBlank(sto, keys);
+        }
+        HQ.isFirstLoad = false; //sto load cuoi se su dung
+    }
+    frmChange();
+    if (_isLoadMaster) {
+        HQ.common.showBusy(false);
+    }
+};
+//trước khi load trang busy la dang load data
+var stoBeforeLoad = function (sto) {
+    HQ.common.showBusy(true, HQ.common.getLang('loadingdata'));
+};
+var grdIN_SiteLocation_BeforeEdit = function (editor, e) {
+    return HQ.grid.checkBeforeEdit(e, keys);
+};
+var grdIN_SiteLocation_Edit = function (item, e) {
+    HQ.grid.checkInsertKey(App.grdIN_SiteLocation, e, keys);
+};
+var grdIN_SiteLocation_ValidateEdit = function (item, e) {
+    return checkValidateEditIN20400(App.grdIN_SiteLocation, e, keys);
+};
+var grdIN_SiteLocation_Reject = function (record) {
+    HQ.grid.checkReject(record, App.grdIN_SiteLocation);
+    frmChange();
+};
+/////////////////////////////////////////////////////////////////////////
+//// Process Data ///////////////////////////////////////////////////////
+var save = function () {
+    if (App.cboSiteID.store.findRecord("SiteId", App.cboSiteID.getValue()) == null)
+    {
+        HQ.message.show(20180605);
+        return;
+    }
+    if (App.frmMain.isValid()) {
+        App.frmMain.submit({
+            timeout: 1800000,
+            waitMsg: HQ.common.getLang("SavingData"),
+            url: 'IN20400/Save',
+            params: {
+                lstIN_SiteLocation: HQ.store.getData(App.stoIN_SiteLocation)
+            },
+            success: function (msg, data) {
+                HQ.message.show(201405071);
+                HQ.isChange = false;
+                refresh("yes");
+            },
+            failure: function (msg, data) {
+                HQ.message.process(msg, data, true);
+            }
+        });
+    }
+};
+
+var deleteData = function (item) {
+    if (item == "yes") {
+        if (HQ.focus == 'header') {
+
+            if (App.frmMain.isValid()) {
+
+                App.frmMain.submit({
+                    waitMsg: HQ.common.getLang("DeletingData"),
+                    url: 'IN20400/DeleteAll',
+                    timeout: 7200,
+
+                    success: function (msg, data) {
+
+                        App.cboSiteID.setValue("");
+
+                    },
+
+                    failure: function (msg, data) {
+
+                        HQ.message.process(msg, data, true);
+
+                    }
+
+                });
+
+            }
+
+
+
+        }
+        else if (HQ.focus == 'grdIN_SiteLocation') {
+            App.grdIN_SiteLocation.deleteSelected();
+            frmChange();
+        }
+
+    }
+};
+
+/////////////////////////////////////////////////////////////////////////
+//// Other Functions ////////////////////////////////////////////////////
+function refresh(item) {
+    if (item == 'yes') {
+        HQ.isChange = false;
+        HQ.isFirstLoad = true;
+        App.stoIN_SiteLocation.reload();
+    }
+};
+
+
+var checkValidateEditIN20400 = function (grd, e, keys, isCheckSpecialChar) {
+    if (keys.indexOf(e.field) != -1) {
+        var regex = /^[a-zA-Z0-9)\(._]+$/
+        if (isCheckSpecialChar == undefined) isCheckSpecialChar = true;
+        if (isCheckSpecialChar) {
+            if (e.value)
+                if (!HQ.util.passNull(e.value) == '' && !HQ.util.passNull(e.value.toString()).match(regex)) {
+                    HQ.message.show(20140811, e.column.text);
+                    return false;
+                }
+        }
+        if (HQ.grid.checkDuplicate(grd, e, keys)) {
+            if (e.column.xtype == "datecolumn")
+                HQ.message.show(1112, Ext.Date.format(e.value, e.column.format));
+            else HQ.message.show(1112, e.value);
+            return false;
+        }
+
+    }
+}
+///////////////////////////////////
+
+
+
+
+
+
+
+
