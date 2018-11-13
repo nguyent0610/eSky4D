25bcca73be9d4b575bb2d723336d181ac06c367e
93dab8b6-8564-4423-a1d4-296d7c0f6e50 <93dab8b6-8564-4423-a1d4-296d7c0f6e50>
2018-11-13 03:04:59 +0000

    src chuần làm cho chuẩn fix theo yêu cầu của VietUc
    TaskID 69398
     
    Fix lỗi màn hình bị trắng
    
    git-tfs-id: [https://hqsofttfs.hqsoft.com.vn/tfs/HQSOFT]$/Cloud/eSky4D;C58790

--- AR/AR10400/AR10400/Global.asax.cs
+++ AR/AR10400/AR10400/Global.asax.cs
@@ -38,14 +38,14 @@ namespace AR10400
             //Session["LangID"] = 1;
 
             Current.Authorize = false;
-            Current.Server = "MARSSVR\\SQL2012";// ConfigurationManager.AppSettings["Server"].ToString();
-            Current.DBSys = "eSky4DSys";//ConfigurationManager.AppSettings["DBSys"].ToString();
+            Current.Server = "TRUONGSAD";// ConfigurationManager.AppSettings["Server"].ToString();
+            Current.DBSys = "KidoTraining_eSky4DSys";//ConfigurationManager.AppSettings["DBSys"].ToString();
             AccessRight acc = new AccessRight();
             acc.Delete = true;
             acc.Insert = true;
             acc.Update = true;
             Session["AR10400"] = acc;
-            Session["DBApp"] = Current.DBApp = "eSky4DApp";// "eBiz4DWebApp";
+            Session["DBApp"] = Current.DBApp = "KidoTraining_eSky4DApp";// "eBiz4DWebApp";
             Session["UserName"] = Current.UserName = "admin";
             Session["CpnyID"] = Current.CpnyID = "HQHD3110";
             Session["Language"] = Current.Language = "en";
