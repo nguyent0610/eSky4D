ae2ffb2b2c23f7b06b68d2dd3c78e926eee0422c
3d421d6a-a81f-4b5f-9953-4f941b6c2f2d <3d421d6a-a81f-4b5f-9953-4f941b6c2f2d>
2019-01-15 10:27:12 +0000

    fix bug 15/1/2019
    git-tfs-id: [https://hqsofttfs.hqsoft.com.vn/tfs/HQSOFT]$/Cloud/eSky4D;C59919

---
+++ OM/OM20320/OM20320/Global.asax
@@ -0,0 +1,1 @@
+<%@ Application Codebehind="Global.asax.cs" Inherits="OM20320.MvcApplication" Language="C#" %>
---
+++ OM/OM20320/OM20320/Global.asax.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using HQ.eSkyFramework;
+using System.Configuration;
+
+namespace OM20320
+{
+    // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
+    // visit http://go.microsoft.com/?LinkId=9394801
+
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+
+            WebApiConfig.Register(GlobalConfiguration.Configuration);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+
+        }
+        protected void Session_Start(object sender, EventArgs e)
+        {
+            Current.Authorize = false;
+            Current.Server = "HOANGSAD";
+            Current.DBSys = "ECO_eSky4DSys";
+            AccessRight acc = new AccessRight();
+            Current.FormatDate = "dd-MM-yyyy";
+            acc.Delete = true;
+            acc.Insert = true;
+            acc.Update = true;
+            Session["OM20320"] = acc;
+            Session["DBApp"] = Current.DBApp = "ECO_eSky4DApp";
+            Session["UserName"] = Current.UserName = "admin";
+            Session["CpnyID"] = Current.CpnyID = "CHHCM0007";
+            Session["Language"] = Current.Language = "vi";
+            Session["LangID"] = 0;
+        }
+    }
+}
